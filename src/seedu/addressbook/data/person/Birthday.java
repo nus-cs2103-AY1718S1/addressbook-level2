2abe8eac668f022210f6514a0f0dd98dc0adcc39
Grant <grantcomiller@gmail.com>
2017-08-28 12:08:17 +0800

    Implemented Code for Birthday for Person

---
+++ src/seedu/addressbook/data/person/Birthday.java
@@ -0,0 +1,55 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Birthday {
+
+    public static final String EXAMPLE = "30,12,1996";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "DD,MM,YYYY";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given birthday.
+     *
+     * @throws IllegalValueException if given birthday string is invalid.
+     */
+    public Birthday(String birthday, boolean isPrivate) throws IllegalValueException {
+        String trimmedBirthday = birthday.trim();
+        this.isPrivate = isPrivate;
+        if (!isValidBirthday(trimmedBirthday)) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.value = trimmedBirthday;
+    }
+
+    /**
+     * Returns true if a given string is a valid person address.
+     */
+    public static boolean isValidBirthday(String test) {
+        return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Birthday // instanceof handles nulls
+                && this.value.equals(((Birthday) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

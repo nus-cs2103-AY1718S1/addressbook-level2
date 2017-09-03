a40c2bb0b152d78db823b8dc728a71205eef9e17
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-03 17:05:26 +0800

    Format the code

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -93,18 +93,14 @@ public class StorageFile {
      * @throws StorageOperationException if there were errors converting and/or storing data to file.
      */
     public void save(AddressBook addressBook) throws StorageOperationException {
-
         /* Note: Note the 'try with resource' statement below.
          * More info: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
          */
-        try (final Writer fileWriter =
-                     new BufferedWriter(new FileWriter(path.toFile()))) {
-
+        try (final Writer fileWriter = new BufferedWriter(new FileWriter(path.toFile()))) {
             final AdaptedAddressBook toSave = new AdaptedAddressBook(addressBook);
             final Marshaller marshaller = jaxbContext.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshaller.marshal(toSave, fileWriter);
-
         } catch (IOException ioe) {
             throw new StorageOperationException("Error writing to file: " + path);
         } catch (JAXBException jaxbe) {

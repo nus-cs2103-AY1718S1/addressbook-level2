700d24598b913c9a219af8eced193a16b68d511a
blaqkrow <joshnfsmw@hotmail.com>
2017-09-04 17:35:01 +0800

    Encapsulated feedbackToUser in CommandResult and IncorrectResult

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,10 +11,11 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private  String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
+
 
     public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
@@ -24,6 +25,12 @@ public class CommandResult {
     public CommandResult(String feedbackToUser, List<? extends ReadOnlyPerson> relevantPersons) {
         this.feedbackToUser = feedbackToUser;
         this.relevantPersons = relevantPersons;
+    }
+    public String getFeedBackToUser() {
+        return feedbackToUser;
+    }
+    public void setFeedBackToUser(String feedback) {
+        this.feedbackToUser=feedback;
     }
 
     /**

9662ad93d6407202a9dd01919f474d7300e84fff
KEVIN\Kevin <kevin2leona@gmail.com>
2017-09-06 07:50:22 +0800

    Implement PostalCode as basic String container

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -1,4 +1,8 @@
 package seedu.addressbook.data.person;
 
 public class PostalCode {
+    private String value;
+    public PostalCode(String postalCode) {
+        this.value = postalCode;
+    }
 }

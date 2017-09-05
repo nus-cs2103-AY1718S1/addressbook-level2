2e640ad22d8afeaf6ce661b39fdd6968ed169d7c
Karrui Lau <karrui.lau@gmail.com>
2017-09-05 18:09:53 +0800

    Address: Fix regex for address validation
    Old regex could cause ArrayIndexOutOfBoundsException and
    exit the program if either {streetName} or {unit} was not
    input.
    New regex will catch these cases and throw
    IllegalValueException instead.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -11,7 +11,7 @@ public class Address {
     public static final String EXAMPLE = "123, some street, some unit number, postal code";
     private static final String MESSAGE_ADDRESS_CONSTRAINTS = "Address has to be in this format: a/BLOCK, STREET, " +
             "UNIT, POSTAL_CODE";
-    private static final String ADDRESS_VALIDATION_REGEX = "\\d{1,5}, .+, \\d{1,6}";
+    private static final String ADDRESS_VALIDATION_REGEX = "\\d+,[A-Za-z,\" 0-9]+[A-Za-z,\"0-9],.+, \\d+";
 
     private final Block blockNumber;
     private final Street streetName;

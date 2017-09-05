1a9f2d975c57a1f9bc757966f2bfafe2182582ea
Jacob Li Peng Cheng <JacobLI@206-191.priv23.nus.edu.sg>
2017-09-05 20:21:26 +0800

    changed format of example

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,6 +9,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
+    public static final String EXAMPLE = "123, CLementi Ave 3, #12-34, 231543";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 

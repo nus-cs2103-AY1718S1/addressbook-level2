429796738167a965cc0b57946ff56fe572227840
KEVIN\Kevin <kevin2leona@gmail.com>
2017-09-06 07:20:30 +0800

    Implement Block class as basic string container

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,4 +1,12 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class Block {
+    
+    public final String value;
+    public Block(String block) {
+        
+        this.value = block;
+    }
 }

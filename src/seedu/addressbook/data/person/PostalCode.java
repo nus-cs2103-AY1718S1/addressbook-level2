ed36a75c206ef019d041d9e870799e53205ca2ab
liuhang0213 <liuhang0213@gmail.com>
2017-09-05 21:20:09 +0800

    Make validation for PostalCode value more permissive and consistent with address validation

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -5,8 +5,8 @@ package seedu.addressbook.data.person;
  */
 public class PostalCode extends AddressComponent{
 
-    // Substring after the 3rd comma, before end of line, consisting of 6 integersd
-    private final static String PARSE_POSTAL_CODE_FROM_ADDRESS_REGEX = "^(?:.*,{3})\\s*([0-9]{6})$";
+    // Substring after the 3rd comma, before end of line
+    private final static String PARSE_POSTAL_CODE_FROM_ADDRESS_REGEX = "^(?:[^,]*,){3}(.*?)$";
 
     public PostalCode(String unitNumber) {
         super(unitNumber);

24c74d5fffe62490ea65b4a3b654b35334a54438
itsdickson <itsklein@hotmail.com>
2017-09-07 01:02:40 +0800

    PostalCode.java: added constructor and main body with methods

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 /**
  * Represents a Person's Address' Postal Code number in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidPostalCode(String)}
@@ -14,4 +16,40 @@ public class PostalCode {
     public final String value;
     private boolean isPrivate;
 
+    public PostalCode(String postalCode, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedPostalCode = postalCode.trim();
+        if (!isValidPostalCode(trimmedPostalCode)) {
+            throw new IllegalValueException(MESSAGE_POSTAL_CONSTRAINTS);
+        }
+        this.value = trimmedPostalCode;
+    }
+
+    /**
+     * Returns true if the given string is a valid postal code.
+     */
+    public static boolean isValidPostalCode(String test) {
+        return test.matches(POSTAL_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+                && this.value.equals(((PostalCode) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }

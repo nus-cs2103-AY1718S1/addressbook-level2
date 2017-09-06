ceda9d9773adacd09fdbaf1b13e4fd1196eea797
Emil Koh <e0014667@u.nus.edu>
2017-09-06 20:51:02 +0800

    added attribute and constructor for Block class

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,4 +1,11 @@
 package seedu.addressbook.data.person;
 
 public class Block {
+    private static String blkNumber = "";
+
+    public Block(String blockNum){
+        blkNumber = blockNum;
+    }
+
+
 }

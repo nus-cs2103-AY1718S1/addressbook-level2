c70fae06ec4db1aff074cdb44598f239ec36f1d0
itsdickson <itsklein@hotmail.com>
2017-09-07 01:00:37 +0800

    PostalCode.java: added example, constraints, regex and variables.

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -6,4 +6,12 @@ package seedu.addressbook.data.person;
  */
 
 public class PostalCode {
+
+    public static final String EXAMPLE = "735654";
+    public static final String MESSAGE_PHONE_CONSTRAINTS = "Person Postal Code number should only contain numbers";
+    public static final String PHONE_VALIDATION_REGEX = "\\d+";
+
+    public final String value;
+    private boolean isPrivate;
+
 }

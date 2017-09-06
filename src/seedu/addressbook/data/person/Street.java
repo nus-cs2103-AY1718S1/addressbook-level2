2216cd5edb5cd2497245f6e87fb0422af8d6537a
itsdickson <itsklein@hotmail.com>
2017-09-07 00:37:22 +0800

    Street.java: added variables, regex, constraints and example

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -6,4 +6,11 @@ package seedu.addressbook.data.person;
  */
 
 public class Street {
+    public static final String EXAMPLE = "Clementi Ave 2";
+    public static final String MESSAGE_STREET_CONSTRAINTS =
+                "Person street should only contain alphabetic characters or numbers";
+    public static final String STREET_VALIDATION_REGEX = "[\\p{Alpha} ]+$";
+
+    public final String value;
+    private boolean isPrivate;
 }

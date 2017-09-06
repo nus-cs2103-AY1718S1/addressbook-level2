f0b6ec98e943f5e61e373960836341e1c5229326
DESKTOP-HPAKHL7\alphe <e0031928@u.nus.edu>
2017-09-06 11:38:27 +0800

    revised address regex format

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,8 +9,9 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person address should be in the following format:" +
+            " BLOCK, STREET, UNIT, POSTAL_CODE";
+    public static final String ADDRESS_VALIDATION_REGEX = "(^\\d+)[,]{1}\\s{1}.+[,]{1}\\s{1}[#]{1}[0-9]+[-]{1}[0-9]+[,]{1}\\s{1}[0-9]+$";
 
     public final String value;
     public Block blockNum;
@@ -26,16 +27,17 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        splitAddress(trimmedAddress);
         this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
+        splitAddress(trimmedAddress);
         this.value = trimmedAddress;
     }
 
     /**
      * Splits the address into block number, street name, unit number and postal code
+     *
      * @param trimmedAddress
      */
     private void splitAddress(String trimmedAddress) {

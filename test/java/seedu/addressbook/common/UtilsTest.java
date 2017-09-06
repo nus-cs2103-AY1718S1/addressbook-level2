f0649e57ae66d667067befcc5f7c70acc0070b37
Dong Shaocong <shaocong.dong@u.nus.edu>
2017-09-06 14:09:39 +0800

    Modify the UtilsTest to add more tests for functions in utils

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,12 +3,84 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.util.TestUtil;
+import seedu.addressbook.util.TypicalPersons;
 
 public class UtilsTest {
+
+    Person person1;
+    Person person2;
+    AddressBook initialAddressBook;
+    List<ReadOnlyPerson> initialList;
+
+    @Before
+    public void setUp() throws IllegalValueException{
+
+        person1 = new Person(new Name("person one"), new Phone("12341234", false),
+                new Email("1@1.com", false),
+                new Block(123), new Street("Kent Ridge 1"),
+                new Unit("#12-34"), new Postal(118430),
+                new UniqueTagList(new HashSet<Tag>()));
+
+        person2 = new Person(new Name("person two"), new Phone("23452345", false),
+                new Email("2@2.com", false),
+                new Block(234), new Street("Kent Ridge 2"),
+                new Unit("#23-45"), new Postal(118430),
+                new UniqueTagList(new HashSet<Tag>()));
+
+        initialAddressBook = new AddressBook();
+        initialAddressBook.addPerson(person1);
+        initialAddressBook.addPerson(person2);
+
+        List<ReadOnlyPerson> initialList = new ArrayList<ReadOnlyPerson>();
+        initialList.add(person1);
+        initialList.add(person2);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void test_create_newAddressBook () {
+        AddressBook newAddressBook = TestUtil.createAddressBook(person1, person2);
+        Assert.assertEquals(initialAddressBook, newAddressBook);
+    }
+
+    @Test
+    public void test_createListOfPeople () {
+        List<ReadOnlyPerson> newList= TestUtil.createList(person1, person2);
+        Assert.assertNotNull(newList);
+        Assert.assertEquals(2, TestUtil.getSize(newList));
+    }
+
+    @Test
+    public void test_generateTestPerson () {
+        Person newPersonGenerated = TestUtil.generateTestPerson();
+        Assert.assertNotNull(newPersonGenerated);
+    }
+
+    @Test
+    public void test_clone_addressBook () {
+        AddressBook newAddressBook = TestUtil.clone(initialAddressBook);
+        Assert.assertEquals(initialAddressBook, newAddressBook);
+    }
+
     @Test
     public void isAnyNull() {
         // empty list

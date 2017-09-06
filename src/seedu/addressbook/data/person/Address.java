c4e65b7a9a55e1fa5c365883b8549fdcb376f10a
Henry Ang <FrozVentus@users.noreply.github.com>
2017-09-06 23:08:53 +0800

    [W4.3d] 7. Shortened code in Address.java

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -26,34 +26,14 @@ public class Address {
         String[] addressComponents = address.split(", ");
 
         this.isPrivate = isPrivate;
-        for (int i = 0; i < addressComponents.length; i++) {
-            if (i == 0) {
-                this.block = new Block(addressComponents[0], isPrivate);
-            }
-            if (i == 1) {
-                this.street = new Street(addressComponents[1], isPrivate);
-            }
-            if (i == 2) {
-                this.unit = new Unit(addressComponents[2], isPrivate);
-            }
-            if (i == 3) {
-                this.postalCode = new PostalCode(addressComponents[3], isPrivate);
-            }
-        }
-        String addressString = new String();
-        if (this.block != null) {
-            addressString = this.block.toString();
-        }
-        if (this.street != null) {
-            addressString = addressString.concat(", " + this.street.toString());
-        }
-        if (this.unit != null) {
-            addressString = addressString.concat(", " + this.unit.toString());
-        }
-        if (this.postalCode != null) {
-            addressString = addressString.concat(", " + this.postalCode.toString());
-        }
-        value = addressString;
+
+        this.block = new Block(addressComponents[0], isPrivate);
+        this.street = new Street(addressComponents[1], isPrivate);
+        this.unit = new Unit(addressComponents[2], isPrivate);
+        this.postalCode = new PostalCode(addressComponents[3], isPrivate);
+
+        value = new String(this.block.toString() + ", " + this.street.toString() + ", " +
+        this.unit.toString() + ", " + this.postalCode);
     }
 
     @Override

98acba80004830758b78af9156ab9373bd894572
Royce <e0030923@u.nus.edu>
2017-09-06 23:22:36 +0800

    Add Street class

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,4 +1,18 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's street in the address book.
+ */
+
 public class Street {
+
+    public final String value;
+
+    public Street (String street) {
+        this.value = street.trim();
+    }
+
+    public String getStreet() {
+        return this.value;
+    }
 }

0b7f1ef1c3583fa704e80106891e640a7bffda6b
itsdickson <itsklein@hotmail.com>
2017-09-07 00:48:38 +0800

    Unit.java: added description

--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -1,4 +1,10 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's Address' unit number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidUnit(String)}
+ */
+
 public class Unit {
+
 }

7e82d2906660f0581b68a5d40f86f453ce3e8cf5
itsdickson <itsklein@hotmail.com>
2017-09-07 00:59:41 +0800

    PostalCode.java: added description

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -1,4 +1,9 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's Address' Postal Code number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPostalCode(String)}
+ */
+
 public class PostalCode {
 }

87d737f574953fba5b2d4f44f6a56b8b24019efe
Emil Koh <e0014667@u.nus.edu>
2017-09-06 20:51:22 +0800

    added attribute and constructor for Street class

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,4 +1,11 @@
 package seedu.addressbook.data.person;
 
 public class Street {
+    private static String streetName = "";
+
+    public Street(String street){
+        streetName = street;
+    }
+
+
 }

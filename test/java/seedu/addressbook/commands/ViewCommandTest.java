e72bf72fbdf2155bb203d84ed07fbd580431bf3b
Alex Ang <E0032221@u.nus.edu>
2017-09-06 13:41:58 +0800

    Update jUnit tests for Address Validation
    Simple amends to formats and errors. All tests should run ok now.

--- test/java/seedu/addressbook/commands/ViewCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewCommandTest.java
@@ -49,7 +49,7 @@ public class ViewCommandTest {
         ReadOnlyPerson stranger = new Person(new Name("me"),
                                              new Phone("123", true),
                                              new Email("some@hey.go", true),
-                                             new Address("nus", false),
+                                             new Address("13, Computing Drive, #01-01, 117538", false),
                                              new UniqueTagList(Collections.emptySet()));
         List<ReadOnlyPerson> listWithExtraPerson
                 = new ArrayList<ReadOnlyPerson>(listWithAllTypicalPersons);

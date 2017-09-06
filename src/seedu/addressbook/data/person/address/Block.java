9c47d478ff9403dbe73292a08a56115a42f3196b
GIDEON-PC\Gideon Fu <gideonfu@outlook.com>
2017-09-07 01:02:45 +0800

    Add 4 new classes for Address: Block, Street, Unit, PostalCode.

---
+++ src/seedu/addressbook/data/person/address/Block.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person.address;
+
+public class Block {
+    public final String blockNumber;
+
+    public Block(String blockNumber) {
+        this.blockNumber = blockNumber;
+    }
+
+}
---
+++ src/seedu/addressbook/data/person/address/Street.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person.address;
+
+public class Street {
+    public final String streetName;
+
+    public Street(String streetName) {
+        this.streetName = streetName;
+    }
+
+}

c2401b5c37c827a73645974a67b5e6ef3cf75680
itsdickson <itsklein@hotmail.com>
2017-09-07 00:26:59 +0800

    Street.java: added description of class

--- src/seedu/addressbook/data/person/Street.java
+++ src/seedu/addressbook/data/person/Street.java
@@ -1,4 +1,9 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's Address' Street in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidStreet(String)}
+ */
+
 public class Street {
 }

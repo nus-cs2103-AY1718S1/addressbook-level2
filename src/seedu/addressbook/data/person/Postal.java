f234982659b519be5b03007c711ad0bc549686a0
Royce <e0030923@u.nus.edu>
2017-09-06 23:23:18 +0800

    Add Postal class

--- src/seedu/addressbook/data/person/Postal.java
+++ src/seedu/addressbook/data/person/Postal.java
@@ -1,4 +1,18 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's block in the address book.
+ */
+
 public class Postal {
+
+    public final String value;
+
+    public Postal (String postal) {
+        this.value = postal.trim();
+    }
+
+    public String getPostal() {
+        return this.value;
+    }
 }

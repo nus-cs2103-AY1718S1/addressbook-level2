15bb083a79cc82de7f8ad25cc346dce9ad3cf0ad
Lenaldnwj <lenald95@hotmail.com>
2017-09-06 16:10:20 +0800

    Change to import java.util.*

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -1,16 +1,12 @@
 package seedu.addressbook.commands;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 

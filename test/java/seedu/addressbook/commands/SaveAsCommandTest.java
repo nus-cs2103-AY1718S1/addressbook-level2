88f4d444a808b507d2796f8ce3c5d970524a297b
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-07 15:37:15 +0800

    Add unit test file

---
+++ test/java/seedu/addressbook/commands/SaveAsCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+
+public class SaveAsCommandTest {
+    @Test
+    public void saveAsCommand_invalidFileExtension_returnedFailureCommandResult() {
+        
+    }
+}

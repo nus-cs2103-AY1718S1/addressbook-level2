02b93245deef3fc526f484ad2b75ea3feca7fa5a
jaivigneshvenugopal <smugs1995@gmail.com>
2017-09-07 23:31:02 +0800

    Broke down Address Class into multiple classes

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+    public String street;
+
+    public Street(String street) {
+        this.street = street;
+    }
+}
---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+public class Unit {
+    public String unit;
+
+    public Unit(String unit) {
+        this.unit = unit;
+    }
+}

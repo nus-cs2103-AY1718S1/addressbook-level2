fcd46f975fa9688b5c3a1b7fcd77d136b09fddc6
JYL123 <li474412210@gmail.com>
2017-09-07 21:30:46 +0800

    Create the last test case for isSimilar

--- test/java/seedu/addressbook/data/NameTest.java
+++ test/java/seedu/addressbook/data/NameTest.java
@@ -16,12 +16,22 @@ public class NameTest {
     private Name name3;
     private Name name4;
 
+    private Name name5;
+    private Name name6;
+
     @Before
     public void setUp() throws Exception {
+        /** Names with upper and lower case*/
         name1 = new Name("emma");
         name2 = new Name("EMMA");
+
+        /** Name that contaons another */
         name3 = new Name("EMMA");
         name4 = new Name("EM");
+
+        /** Names with different order */
+        name5 = new Name("EMMA");
+        name6 = new Name("EAM");
     }
 
     @Rule
@@ -31,6 +41,7 @@ public class NameTest {
     public void isSimilar() {
         assertTrue(name1.isSimilar(name2));
         assertTrue(name3.isSimilar(name4));
+        assertTrue(name5.isSimilar(name6));
     }
 
 }

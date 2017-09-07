9e46d86b2c8576fd07aba9bb959d44d0d2bab27f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-07 15:53:31 +0800

    Finish the first test for saveAsCommand

--- test/java/seedu/addressbook/commands/SaveAsCommandTest.java
+++ test/java/seedu/addressbook/commands/SaveAsCommandTest.java
@@ -1,9 +1,12 @@
 package seedu.addressbook.commands;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import seedu.addressbook.storage.StorageFile;
+
+import static org.junit.Assert.assertEquals;
 
 public class SaveAsCommandTest {
     private static final String TEST_DATA_FOLDER = "test/data/StorageFileTest";
@@ -12,12 +15,22 @@ public class SaveAsCommandTest {
 
     private StorageFile storage = null;
 
-    @Rule
-    private ExpectedException thrown = ExpectedException.none();
+    @Before
+    public void setup() throws Exception {
+        storage = new StorageFile(TEST_DATA_FOLDER + "/" + VALID_FILE_NAME);
+    }
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /***
+     * Returns a failure message (rather than throws any exception) when the file extension is invalid.
+     */
     @Test
     public void saveAsCommand_invalidFileExtension_returnsIncorrect() {
         SaveAsCommand command = new SaveAsCommand("some_path.txt");
-        CommandResult result = command.execute();
+        CommandResult result = command.execute(storage);
+
+        assertEquals(result.feedbackToUser, StorageFile.MESSAGE_INVALID_STORAGE_PATH);
     }
 }

732c8b72c2db1f3a9d4da0ce63539b5ad698bab1
DESKTOP-9RFF1NR\Ernest Ong YuHeng <E0031105@u.nus.edu>
2017-09-08 00:53:36 +0800

    Added new classes Block, Street, Unit, Postal_Code No connection made between new classes and Address yet. No other updates yet.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -57,3 +57,127 @@ public class Address {
         return isPrivate;
     }
 }
+
+public class Block{
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Block(String block, boolean isPrivate) {
+        String trimmedBlock = block.trim();
+        this.isPrivate = isPrivate;
+        this.value = trimmedBlock;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Block // instanceof handles nulls
+                && this.value.equals(((Block) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
+
+public class Street{
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Street(String street, boolean isPrivate) {
+        String trimmedStreet = street.trim();
+        this.isPrivate = isPrivate;
+        this.value = trimmedStreet;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Street // instanceof handles nulls
+                && this.value.equals(((Street) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+
+
+    public boolean isPrivate() { return isPrivate; }
+}
+
+public class Unit{
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Unit(String unit, boolean isPrivate) {
+        String trimmedUnit = unit.trim();
+        this.isPrivate = isPrivate;
+        this.value = trimmedUnit;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Unit // instanceof handles nulls
+                && this.value.equals(((Unit) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+
+
+    public boolean isPrivate() { return isPrivate; }
+}
+
+public class Postal_Code{
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Postal_Code(String postCode, boolean isPrivate) {
+        String trimmedPostCode = postCode.trim();
+        this.isPrivate = isPrivate;
+        this.value = trimmedPostCode;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Postal_Code // instanceof handles nulls
+                && this.value.equals(((Postal_Code) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+
+
+    public boolean isPrivate() { return isPrivate; }
+}

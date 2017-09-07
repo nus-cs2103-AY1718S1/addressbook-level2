3ea54ad997f8f68d0f335126cc4ee3e12654567b
antonchua <emanasaur@gmail.com>
2017-09-07 15:31:53 +0800

    Add skeleton class structure to class Block

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,4 +1,14 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a Person's block (address) in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+
 public class Block {
+    private int bNum;
+    public Block(int bNum){
+
+    }
+
 }

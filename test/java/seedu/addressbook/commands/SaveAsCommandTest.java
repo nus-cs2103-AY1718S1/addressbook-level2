f7d6f2ab5aa9ab37443bda1ea09918eb57d8c629
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-07 15:49:22 +0800

    Some setup for saveAsCommand test

--- test/java/seedu/addressbook/commands/SaveAsCommandTest.java
+++ test/java/seedu/addressbook/commands/SaveAsCommandTest.java
@@ -1,10 +1,23 @@
 package seedu.addressbook.commands;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seedu.addressbook.storage.StorageFile;
 
 public class SaveAsCommandTest {
+    private static final String TEST_DATA_FOLDER = "test/data/StorageFileTest";
+    private static final String VALID_FILE_NAME = "ValidData.xml";
+    private static final String INVALID_FILE_EXTENSION = "some_path.txt";
+
+    private StorageFile storage = null;
+
+    @Rule
+    private ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void saveAsCommand_invalidFileExtension_returnedFailureCommandResult() {
-        
+    public void saveAsCommand_invalidFileExtension_returnsIncorrect() {
+        SaveAsCommand command = new SaveAsCommand("some_path.txt");
+        CommandResult result = command.execute();
     }
 }

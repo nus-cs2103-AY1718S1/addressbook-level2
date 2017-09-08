27a78048fb043852e28e1843267d2e9e7cdacc37
Grant <grantcomiller@gmail.com>
2017-09-08 18:28:42 +0800

    Refactored wildcard imports to only include necessary classes

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -4,7 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.*;
+import seedu.addressbook.data.person.Birthday;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList;
+
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
 
@@ -41,6 +49,7 @@ public class AddCommand extends Command {
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
         }
+
         this.toAdd = new Person(
                 new Name(name),
                 new Phone(phone, isPhonePrivate),

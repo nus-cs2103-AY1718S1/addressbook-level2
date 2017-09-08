0212c373cb36d4188fef29cce448b314d9eaf255
Henry Ang <FrozVentus@users.noreply.github.com>
2017-09-08 16:14:14 +0800

    [W5.3b] 1. Extracted isPrivate attribute into superclass Contact.java

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    protected boolean isPrivate;
+
+    public Contact() {
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

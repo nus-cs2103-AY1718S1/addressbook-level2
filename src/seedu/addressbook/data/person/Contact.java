5b3ee711bd619eed1c17857febf72515a1b94b17
namjjd <namjjd@gmail.com>
2017-09-08 21:32:10 +0800

    Add contact class as parent class to address, contact and phone class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,8 +1,8 @@
 package seedu.addressbook.data.person;
 
 public class Contact {
-    public final String value;
-    private boolean isPrivate;
+    public String value;
+    protected boolean isPrivate;
 
     public Contact(String value, boolean isPrivate) {
         this.value = value;

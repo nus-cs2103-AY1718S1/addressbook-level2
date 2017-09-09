42ed89c0613a72950d1515484783602bc621a31f
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-09-09 20:38:59 +0800

    [W5.4][W09-A4] Set up comments for JUnit tests

--- test/java/seedu/addressbook/parser/ParserTest.java
+++ test/java/seedu/addressbook/parser/ParserTest.java
@@ -12,17 +12,7 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 
-import seedu.addressbook.commands.AddCommand;
-import seedu.addressbook.commands.ClearCommand;
-import seedu.addressbook.commands.Command;
-import seedu.addressbook.commands.DeleteCommand;
-import seedu.addressbook.commands.ExitCommand;
-import seedu.addressbook.commands.FindCommand;
-import seedu.addressbook.commands.HelpCommand;
-import seedu.addressbook.commands.IncorrectCommand;
-import seedu.addressbook.commands.ListCommand;
-import seedu.addressbook.commands.ViewAllCommand;
-import seedu.addressbook.commands.ViewCommand;
+import seedu.addressbook.commands.*;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
@@ -89,7 +79,7 @@ public class ParserTest {
     }
 
     /*
-     * Tests for ingle index argument commands ===============================================================
+     * Tests for single index argument commands ===============================================================
      */
 
     @Test
@@ -294,6 +284,14 @@ public class ParserTest {
         }
         return addCommand;
     }
+
+
+    /*
+     * Tests for sort person command ==============================================================================
+     */
+
+    @Test
+
 
     /*
      * Utility methods ====================================================================================

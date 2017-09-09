932080ea06a8975acc7bfcc1c489f760b08e13e8
Karrui Lau <karrui.lau@gmail.com>
2017-09-09 18:59:23 +0800

    Address: Add more restrictive regex validation check
    Old regex check allowed an invalid address string where the
    form: "111, 1 Downing Street, 1-123, 111111, nonsense" was
    allowed.
    
    This commit fixes that bug.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -11,7 +11,7 @@ public class Address {
     public static final String EXAMPLE = "123, some street, some unit number, postal code";
     private static final String MESSAGE_ADDRESS_CONSTRAINTS = "Address has to be in this format: a/BLOCK, STREET, " +
             "UNIT, POSTAL_CODE";
-    private static final String ADDRESS_VALIDATION_REGEX = "\\d+,[A-Za-z,\" 0-9]+[A-Za-z,\"0-9],.+, \\d+";
+    private static final String ADDRESS_VALIDATION_REGEX = "\\d+,[A-Za-z 0-9]+[A-Za-z 0-9], #?\\d*-?\\d*, \\d{6}";
 
     private static final int BLOCK_NUMBER_INDEX = 0;
     private static final int STREET_NAME_INDEX = 1;

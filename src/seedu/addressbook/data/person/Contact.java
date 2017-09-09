21a622e235906b7f06dd103eb8acf8a33e2f3dc7
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-09 12:02:47 +0800

    Add Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Abstracts the common patterns of contact information for a specific person.
+ *
+ * @author Niu Yunpeng
+ */
+public class Contact {
+
+}

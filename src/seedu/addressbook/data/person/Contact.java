a08ba0dd7f0398c265d3a5621c37dc35cd800ea1
Kennard Ng <Kennard_ng@outlook.com>
2017-09-09 17:48:19 +0800

    add parent class Contact.java and convert Email.java and Phone.java to inherit from Contact.java

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,60 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's contact in the address book. Parent class of Email, Phone
+ * and Address classes.
+ * Guarantees: immutable; is valid as declared in {@link #isValidContact(String)}
+ */
+public abstract class Contact {
+	public static String EXAMPLE;
+	public static String MESSAGE_CONSTRAINTS = "Invalid Contact format";
+	public static String VALIDATION_REGEX;
+
+	public String value;
+	private boolean isPrivate;
+
+	/**
+	 * Validates given contact.
+	 *
+	 * @throws IllegalValueException if given contact string is invalid.
+	 */
+	public void setContact(String contact, boolean isPrivate) throws IllegalValueException {
+		this.isPrivate = isPrivate;
+		String trimmedContact = contact.trim();
+		if (!isValidContact(contact)) {
+			throw new IllegalValueException(MESSAGE_CONSTRAINTS);
+		}
+
+		this.value = trimmedContact;
+	}
+
+	/**
+	 * Returns true if the given string is a valid contact.
+	 */
+	public static boolean isValidContact(String contact) {
+		return contact.matches(VALIDATION_REGEX);
+	};
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof Email // instanceof handles nulls
+				&& this.value.equals(((Email) other).value)); // state check
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
+
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+}

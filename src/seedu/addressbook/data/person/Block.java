6015411bee80c2554926bf5f75357f81fdd88690
JunQuann <limjunquann@gmail.com>
2017-09-09 18:18:55 +0800

    Split Address class into Block, Street, Unit and Postal_Code

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+
+    public String blockNumber;
+
+    public Block(String blockNumber) {
+        this.blockNumber = blockNumber;
+    }
+
+    public String getBlockNumber() {
+        return blockNumber;
+    }
+
+}
---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+
+    public String streetName;
+
+    public Street(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+}

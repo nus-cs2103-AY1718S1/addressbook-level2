4c6734991a76319b53421ab88d4306ad53bb48ae
kengying <L.kengying@gmail.com>
2017-09-10 01:38:07 +0800

    edit message usage

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -13,7 +13,7 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Displays all persons in the address book in alphabetical order as a list with index numbers.\n"
             + "Example: " + COMMAND_WORD;
 
 

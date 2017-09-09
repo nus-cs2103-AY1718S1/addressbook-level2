3ed76dafb59a07be04cbbe4b4a20eee618f9244d
Emil Koh <e0014667@u.nus.edu>
2017-09-09 19:56:55 +0800

    completion of SortCommand class and credit to owner of original idea and methodology

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,16 +1,20 @@
 package seedu.addressbook.commands;
 
+//Inspiration and guidance from Mr Henry Ang
+
 public class SortCommand extends Command{
 
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list sorted alphabetically.\n"
+            + ": Displays all persons in the address book as a list sorted in lexicographic order.\n"
             + "Example: " + COMMAND_WORD;
 
+    public static final String MESSAGE_SUCCESS = "List sorted successfully";
 
     @Override
     public CommandResult execute() {
-        addressBook.executeSort();
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }

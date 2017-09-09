a40bf031daa433609b1ef11ae942a4b6928a76e8
Xiang Hailin <895227442@qq.com>
2017-09-09 20:38:52 +0800

    finish LO-2KLoC, add editing function

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -66,6 +66,22 @@ public class Person implements ReadOnlyPerson {
         return new UniqueTagList(tags);
     }
 
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     public int getSequenceNumber() {
         return sequenceNumber;
     }

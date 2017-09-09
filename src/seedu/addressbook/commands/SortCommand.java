51bab5f0c8c10d311751ea47c727281d65519f08
stan <stanley_1995@hotmail.com>
2017-09-10 04:30:02 +0800

    add new SortCommand Class

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.commands;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+public class SortCommand {
+
+    public SortCommand(){
+
+    }
+}
+

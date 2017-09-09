19b1ab3b34ec555bfe2d7f48cd0aa7245646a3f5
kengying <L.kengying@gmail.com>
2017-09-10 02:22:19 +0800

    add in sort function in SortCommand.java

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,6 +2,9 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -17,9 +20,29 @@ public class SortCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
 
+    private static List SortCommand(List<ReadOnlyPerson> allPersons) {
+        List<ReadOnlyPerson> toBeDisplayed = new ArrayList<ReadOnlyPerson>(allPersons);
+        ObjectComparator comparator = new ObjectComparator();
+        Collections.sort(toBeDisplayed, comparator);
+        return toBeDisplayed;
+    }
+
+
     @Override
     public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        List<ReadOnlyPerson> toBeDisplayed = SortCommand(allPersons);
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), toBeDisplayed);
     }
+}
+
+
+ class ObjectComparator implements Comparator<ReadOnlyPerson> {
+
+    public int compare(ReadOnlyPerson obj1, ReadOnlyPerson obj2) {
+        String person1 = obj1.getName().toString();
+        String person2 = obj2.getName().toString();
+        return person1.compareToIgnoreCase(person2);
+    }
+
 }

a237c13dd7b939292e190a832dab9425d9deaae2
Unknown <zenghoulim@outlook.com>
2017-09-10 01:26:17 +0800

    UtilTest: Add test for missing method
    - To achieve learning objective [Baisc JUnit Testing]
    - Added a test to test for UtilsiAnyNull(Objects...)

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,12 +3,30 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.omg.CORBA.OBJ_ADAPTER;
 
 public class UtilsTest {
+    @Test
+    public void isAnyNull() throws Exception {
+        // no object
+        assertAreNotNull();
+
+        // all are non-null objects
+        assertAreNotNull(new Integer(1), "abc");
+        assertAreNotNull(new ArrayList<String>());
+
+        // at least one is a null object
+        String[] stringArrayWithoutObjects = new String[3]; // array contains 3 null objects
+        assertAreNull(stringArrayWithoutObjects[2], new Integer(2), "abc");
+        assertAreNull(stringArrayWithoutObjects[0]);
+        assertAreNull(stringArrayWithoutObjects[0], null);
+
+    }
 
 
     @Test
@@ -38,6 +56,15 @@ public class UtilsTest {
 
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+
+    private void assertAreNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertAreNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
     }
 
     private void assertNotUnique(Object... objects) {

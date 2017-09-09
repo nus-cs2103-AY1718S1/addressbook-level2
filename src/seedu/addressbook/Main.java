60ec92a9bf1b4da2fd151922f33e61812a4bf975
Aaron Soh <aaronyhs0h@gmail.com>
2017-09-09 20:40:23 +0800

    'Original'

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -14,14 +14,12 @@ import seedu.addressbook.storage.StorageFile;
 import seedu.addressbook.storage.StorageFile.InvalidStorageFilePathException;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 import seedu.addressbook.ui.TextUi;
-Testing
 
 /**
  * Entry point of the Address Book application.
  * Initializes the application and starts the interaction with the user.
  */
 public class Main {
-    Test Test
     /** Version info of the program. */
     public static final String VERSION = "AddressBook Level 2 - Version 1.0";
 

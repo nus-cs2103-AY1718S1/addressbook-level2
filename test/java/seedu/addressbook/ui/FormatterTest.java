9175d5d7aae686484c5dd365fa13c560f210682d
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 11:55:43 +0800

    Use reflection to test private method

--- test/java/seedu/addressbook/ui/FormatterTest.java
+++ test/java/seedu/addressbook/ui/FormatterTest.java
@@ -1,4 +1,24 @@
 package seedu.addressbook.ui;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
 public class FormatterTest {
+
+    @Test
+    public void formatter_printIndexItem() throws Exception {
+        Class formatterClass = Class.forName("seedu.addressbook.ui.Formatter");
+        Class<?>[] paramTypes = {int.class, String.class};
+
+        Method method = formatterClass.getDeclaredMethod("getIndexedListItem", paramTypes);
+        method.setAccessible(true);
+
+        String result = (String) method.invoke(formatterClass.newInstance(), 1, "something");
+        assertEquals("\t1. something", result);
+    }
 }

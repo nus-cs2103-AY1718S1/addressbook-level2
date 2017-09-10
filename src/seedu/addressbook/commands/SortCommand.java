04137b762bc92e3d2b93ba54c8d85e476b7fb5fa
ALPHEUS\Alpheus Lim <e0031928@u.nus.edu>
2017-09-10 18:05:32 +0800

    added sort command

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+public class SortCommand extends Command{
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts list in alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sortBook();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
+
+

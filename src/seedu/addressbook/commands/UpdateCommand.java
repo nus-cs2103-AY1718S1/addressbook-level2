9f30cf971e1b2315c9a7215147716cdffe4465a7
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 13:43:35 +0800

    Update help command and fix IO test

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -9,7 +9,7 @@ public class UpdateCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Updates the contact details of one existing person in the AddressBook.\n"
             + "The person is identified by the index number in the last shown person listing.\n"
-            + "One or multiple contact details of that person can be updated at one time"
+            + "One or multiple contact details of that person can be updated at one time.\n"
             + "Contact details can be marked private by prepending 'p' to the prefix.\n"
             + "Parameters: INDEX [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD + " 1 p/86492434 e/someguy@gmail.com";

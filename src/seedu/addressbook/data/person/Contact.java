1c3426dd31b14ba7c015fc31b3c7d12696a3f886
JunQuann <limjunquann@gmail.com>
2017-09-10 13:07:56 +0800

    Extracted commonalities from Phone, Email and Address classes into a parent class called Contact.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    public String value;
+    protected boolean isPrivate;
+
+    public Contact(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+}

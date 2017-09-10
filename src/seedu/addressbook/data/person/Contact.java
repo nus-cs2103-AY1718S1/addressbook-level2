3a8ef6463e34de44bd031cb4fccfc04ad82b2c32
Zames <zames.chua@gmail.com>
2017-09-11 00:59:49 +0800

    Add Contact as a superclass of Address, Phone, and Email

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,47 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's email in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
+ */
+public class Contact {
+
+    public String value;
+    private boolean isPrivate;
+
+    public Contact() {
+
+    }
+
+    protected void setValue(String value) {
+        this.value = value;
+    }
+
+    protected void setPrivacy(boolean privacy) {
+        this.isPrivate = privacy;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

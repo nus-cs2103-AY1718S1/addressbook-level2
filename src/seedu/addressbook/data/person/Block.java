8f370e03ad0544e580652e3e0d5a455b1775de94
HanYaodong <hanyaodongcn@hotmail.com>
2017-09-10 12:36:50 +0800

    Split address to four classes Create dummy class file and implement the Block class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,53 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ *
+ */
+class Block {
+
+    public static final String EXAMPLE = "123";
+    public static final String MESSAGE_BLCOK_CONSTRAINTS = "Block should only contain numbers";
+    public static final String BLOCK_VALIDATION_REGEX = "\\d+";
+
+    private final String value;
+
+    public Block(String block) throws IllegalValueException {
+        String trimmedBlock = block.trim();
+        if (!isValidBlock(trimmedBlock))
+            throw new IllegalValueException(MESSAGE_BLCOK_CONSTRAINTS);
+        this.value = trimmedBlock;
+    }
+
+    private boolean isValidBlock(String test) {
+        return test.matches(BLOCK_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other
+                || (other instanceof Block
+                && this.value.equals(((Block) other).value));
+    }
+
+    /**
+     * Returns the hash code of the Block
+     *
+     * @return hash code of the block
+     */
+    @Override
+    public int hashCode() {
+        // uses block number as hash code
+        return Integer.parseInt(value);
+    }
+
+
+
+
+}

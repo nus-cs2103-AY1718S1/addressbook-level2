388ebecff1673ceec9818f98b3249026b1e8fab6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 11:57:50 +0800

    Refactor the FormatterTest class

--- test/java/seedu/addressbook/ui/FormatterTest.java
+++ test/java/seedu/addressbook/ui/FormatterTest.java
@@ -9,16 +9,23 @@ import java.lang.reflect.Method;
 import static org.junit.Assert.assertEquals;
 
 public class FormatterTest {
+    Formatter formatter;
+    Class formatterClass;
+
+    @Before
+    public void setUp() {
+        formatter = new Formatter();
+        formatterClass = formatter.getClass();
+    }
 
     @Test
     public void formatter_printIndexItem() throws Exception {
-        Class formatterClass = Class.forName("seedu.addressbook.ui.Formatter");
         Class<?>[] paramTypes = {int.class, String.class};
 
         Method method = formatterClass.getDeclaredMethod("getIndexedListItem", paramTypes);
         method.setAccessible(true);
 
-        String result = (String) method.invoke(formatterClass.newInstance(), 1, "something");
+        String result = (String) method.invoke(formatter, 1, "something");
         assertEquals("\t1. something", result);
     }
 }

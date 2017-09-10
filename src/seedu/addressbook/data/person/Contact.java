5b35f184ce27a210ba1e197aa8e6c964c9c6853f
JYL123 <li474412210@gmail.com>
2017-09-10 10:47:54 +0800

    Add common methods from Email, Phone, and Address

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -17,4 +17,25 @@ public class Contact {
         this.value = value;
         this.isPrivate = isPrivate;
     }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }

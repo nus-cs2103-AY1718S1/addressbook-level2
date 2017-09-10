377b0072da6b07a16b37d5c9cac69526ba071994
DESKTOP-7SUIEBU\Jasmine <jgreenteaj@gmail.com>
2017-09-10 13:18:05 +0800

    attempt to resolve conflict

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -9,6 +9,40 @@ import java.util.List;
 import org.junit.Test;
 
 public class UtilsTest {
+
+    @Test
+    public void elementsAreUnique() throws Exception {
+        // empty list
+        assertAreUnique();
+
+        // only one object
+        assertAreUnique((Object) null);
+        assertAreUnique(1);
+        assertAreUnique("");
+        assertAreUnique("abc");
+
+        // all objects unique
+        assertAreUnique("abc", "ab", "a");
+        assertAreUnique(1, 2);
+
+        // some identical objects
+        assertNotUnique("abc", "abc");
+        assertNotUnique("abc", "", "abc", "ABC");
+        assertNotUnique("", "abc", "a", "abc");
+        assertNotUnique(1, new Integer(1));
+        assertNotUnique(null, 1, new Integer(1));
+        assertNotUnique(null, null);
+        assertNotUnique(null, "a", "b", null);
+    }
+
+    private void assertAreUnique(Object... objects) {
+        assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    private void assertNotUnique(Object... objects) {
+        assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
     @Test
     public void isAnyNull()throws Exception{
         // empty list
@@ -50,38 +84,5 @@ public class UtilsTest {
         // confirms nulls inside the list are not considered
         List<Object> nullList = Arrays.asList((Object) null);
         assertFalse(Utils.isAnyNull(nullList));
-    }
-
-    @Test
-    public void elementsAreUnique() throws Exception {
-        // empty list
-        assertAreUnique();
-
-        // only one object
-        assertAreUnique((Object) null);
-        assertAreUnique(1);
-        assertAreUnique("");
-        assertAreUnique("abc");
-
-        // all objects unique
-        assertAreUnique("abc", "ab", "a");
-        assertAreUnique(1, 2);
-
-        // some identical objects
-        assertNotUnique("abc", "abc");
-        assertNotUnique("abc", "", "abc", "ABC");
-        assertNotUnique("", "abc", "a", "abc");
-        assertNotUnique(1, new Integer(1));
-        assertNotUnique(null, 1, new Integer(1));
-        assertNotUnique(null, null);
-        assertNotUnique(null, "a", "b", null);
-    }
-
-    private void assertAreUnique(Object... objects) {
-        assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
-    }
-
-    private void assertNotUnique(Object... objects) {
-        assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
 }

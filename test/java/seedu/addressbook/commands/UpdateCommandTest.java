d18d1acb2bad5143ff54d12c836a994883b106e7
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 17:05:10 +0800

    Give the setup of Junit test

--- test/java/seedu/addressbook/commands/UpdateCommandTest.java
+++ test/java/seedu/addressbook/commands/UpdateCommandTest.java
@@ -1,13 +1,19 @@
 package seedu.addressbook.commands;
 
 import org.junit.Before;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.Person;
 import seedu.addressbook.util.TypicalPersons;
 
 public class UpdateCommandTest {
     private TypicalPersons td;
+    private AddressBook addressBook;
+    private Person[] persons;
 
     @Before
     public void setUp() {
-
+        td = new TypicalPersons();
+        addressBook = td.getTypicalAddressBook();
+        persons = td.getTypicalPersons();
     }
 }

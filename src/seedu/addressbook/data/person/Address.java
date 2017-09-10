9272c87c9876be3002ca9380a3c10d873b8e57ee
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-09-10 14:36:13 +0800

    Edit  Address.java to split address into 4 class : block, street name, unit number, postal code

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,11 +9,17 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should be in this format :" +
+                                                            "Block, Street Name, Unit Number, Postal Code";
+    public static final String ADDRESS_VALIDATION_REGEX = "[A-Za-z0-9], \\s.+, \\s.+, \\s\\d+";
 
     public final String value;
     private boolean isPrivate;
+    
+    public final Block BLOCK = new Block("");
+    public final Street STREET_NAME = new Street("");
+    public final Unit UNIT_NUMBER = new Unit("");
+    public final PostalCode POSTAL_CODE = new PostalCode("");
 
     /**
      * Validates given address.
@@ -26,9 +32,24 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
+        
+        parseAddress(trimmedAddress);
+        
         this.value = trimmedAddress;
     }
 
+    /**
+     * Parse given address.
+     */
+    public void parseAddress(String trimmedAddress) {
+        String[] parsedAddress = trimmedAddress.split(", ");
+        
+        BLOCK.setBlock(parsedAddress[0]);
+        STREET_NAME.setStreetName(parsedAddress[1]);
+        UNIT_NUMBER.setUnitNumber(parsedAddress[2]);
+        POSTAL_CODE.setPostalCode(parsedAddress[3]);
+    }
+    
     /**
      * Returns true if a given string is a valid person address.
      */
@@ -38,7 +59,7 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return "" + BLOCK + " " + STREET_NAME + " " + UNIT_NUMBER + " " + POSTAL_CODE;
     }
 
     @Override

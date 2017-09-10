fa950cc11bf5887c9d5d974a9bc886211d146ee0
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 13:29:46 +0800

    Add execute method to update command

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
 public class UpdateCommand extends Command {
     public static final String COMMAND_WORD = "update";
 
@@ -10,4 +13,23 @@ public class UpdateCommand extends Command {
             + "Contact details can be marked private by prepending 'p' to the prefix.\n"
             + "Parameters: INDEX [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD + " 1 p/86492434 e/someguy@gmail.com";
+
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+
+    public UpdateCommand(int targetVisibleIndex) {
+        super(targetVisibleIndex);
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            if (!addressBook.containsPerson(target)) {
+                return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+            }
+            return new CommandResult(String.format(MESSAGE_SUCCESS));
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+    }
 }

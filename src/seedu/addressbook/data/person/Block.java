90b8779d502a1488fd3c205aaa191be2b48dcf6a
Sarah Ng <sarah@r-168-98-25-172.comp.nus.edu.sg>
2017-09-10 11:34:50 +0800

    W3.4d add Block.java, Street.java, Unit.java and PostalCode.java

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+    public String value;
+
+    public Block(String block) {
+        this.value = block;
+    }
+
+    public String toString() {
+        return value;
+    }
+}
---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Unit {
+    public String value;
+
+    public Unit(String unit) {
+        this.value = unit;
+    }
+
+    public String toString() {
+        return value;
+    }
+}

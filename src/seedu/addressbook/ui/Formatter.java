35d32957d870e55a929bb84d3e14f3b530d0c3b4
Wang Junming <e0134140@u.nus.edu>
2017-09-10 18:17:38 +0800

    Exract out the responsibility of fomatting text for display to class Formatter

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,112 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+import static seedu.addressbook.common.Messages.*;
+
+public class Formatter {
+
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    private static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    /** Format of a comment input line. Comment lines are silently consumed when reading user input. */
+    private static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
+
+    /**
+     * Returns true if the user input line should be ignored.
+     * Input should be ignored if it is parsed as a comment, is only whitespace, or is empty.
+     *
+     * @param rawInputLine full raw user input line.
+     * @return true if the entire user input line should be ignored.
+     */
+    public boolean shouldIgnore(String rawInputLine) {
+        return rawInputLine.trim().isEmpty() || isCommentLine(rawInputLine);
+    }
+
+
+    /**
+     * Returns true if the user input line is a comment line.
+     *
+     * @param rawInputLine full raw user input line.
+     * @return true if input line is a comment.
+     */
+    public boolean isCommentLine(String rawInputLine) {
+        return rawInputLine.trim().matches(COMMENT_LINE_FORMAT_REGEX);
+    }
+
+    /** Formats a list of strings as a viewable indexed list. */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+
+    public String getEnterCmdMessage() {
+        return LINE_PREFIX + "Enter command: ";
+    }
+
+    public String getEnteredCommand(String fullInputLine) {
+        return "[Command entered:" + fullInputLine + "]";
+    }
+
+    public String[] getWelcomeMessage(String version, String storageFilePath) {
+        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
+        String[] message =
+                {
+                DIVIDER,
+                DIVIDER,
+                MESSAGE_WELCOME,
+                version,
+                MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE,
+                storageFileInfo,
+                DIVIDER};
+
+        return message;
+    }
+
+    public String[] getGoodByeMessage() {
+        String[] message = {MESSAGE_GOODBYE, DIVIDER, DIVIDER};
+        return message;
+    }
+
+    public String[] getInitFailedMessage() {
+        String[] message = {MESSAGE_INIT_FAILED, DIVIDER, DIVIDER};
+        return message;
+    }
+
+    public String formatting(String m) {
+        return LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX);
+    }
+
+    public String[] getFormatResult(String feedbackToUser) {
+        String[] message = {feedbackToUser, DIVIDER};
+        return message;
+    }
+
+}

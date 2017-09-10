b8b6ddd605a06c1305186aafa0c7d9f05a894c2a
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-10 17:18:05 +0800

    Add test case

--- test/java/seedu/addressbook/commands/UpdateCommandTest.java
+++ test/java/seedu/addressbook/commands/UpdateCommandTest.java
@@ -1,19 +1,35 @@
 package seedu.addressbook.commands;
 
 import org.junit.Before;
+import org.junit.Test;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.util.TypicalPersons;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class UpdateCommandTest {
-    private TypicalPersons td;
     private AddressBook addressBook;
     private Person[] persons;
 
     @Before
     public void setUp() {
-        td = new TypicalPersons();
+        TypicalPersons td = new TypicalPersons();
         addressBook = td.getTypicalAddressBook();
         persons = td.getTypicalPersons();
+    }
+
+    @Test
+    public void updateName() throws Exception {
+        Person firstPerson = persons[0];
+        UpdateCommand command = new UpdateCommand(1, "Amy Steven",
+                null, false, null, false,
+                null, false, Collections.emptySet());
+        command.setData(addressBook, Arrays.asList(persons));
+        command.execute();
+        assertEquals("Amy Steven", firstPerson.getName().toString());
     }
 }

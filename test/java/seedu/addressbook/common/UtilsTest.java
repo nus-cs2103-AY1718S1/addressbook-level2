c317def0c599de48c3ee834c57cfc7c0df32440e
Esilocke <klin_zw@hotmail.com>
2017-09-11 23:46:29 +0800

    JUnit: Added tests for isAnyNull
    Added JUnit tests to verify that isAnyNull is working correctly.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,11 +1,14 @@
 package seedu.addressbook.common;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.addressbook.common.Utils.isAnyNull;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilsTest {
@@ -42,5 +45,22 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    @Test
+    /**
+     * Asserts that null objects are properly captured by isAnyNull.
+     */
+    public  void assertIsAnyNull(){
+        boolean noObjectsTest = isAnyNull();
+        boolean emptyObjectTest = isAnyNull(" ", " ", 0);
+        boolean noNullObjectTest = isAnyNull("Item1", "Item2", 123, 33.4, 'a');
+        boolean nullPresentTest = isAnyNull("Item1", null, 123, 33.4);
+        boolean allNullTest = isAnyNull(null, null, null, null);
+
+        boolean[] testResults = {noObjectsTest, emptyObjectTest, noNullObjectTest, nullPresentTest, allNullTest};
+        boolean[] expected = {false, false, false, true, true};
+
+        Assert.assertArrayEquals(testResults, expected);
     }
 }

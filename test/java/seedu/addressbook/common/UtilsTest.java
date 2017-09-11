58950318cadb33b85e709b6815377213672294b6
Alex Ang <E0032221@u.nus.edu>
2017-09-11 21:43:54 +0800

    Add tests for Utils.isAnyNull(Object...) method
    For LO-JUnit. Note: When using literal 'null', Java doesn't know if it is a null Object or null Object array. Java assumes the latter for single-argument null.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -2,14 +2,61 @@ package seedu.addressbook.common;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
 public class UtilsTest {
 
+
+    @Test
+    public void isAnyNull() {
+        // no arguments
+        assertFalse(Utils.isAnyNull());
+        
+        // non-empty arguments with generic Java Object()s
+        assertFalse(Utils.isAnyNull(new Object()));
+        assertFalse(Utils.isAnyNull(new Object(), new Object()));
+        assertFalse(Utils.isAnyNull(new Object(), new Object(), new Object()));
+        assertFalse(Utils.isAnyNull(""));
+        assertFalse(Utils.isAnyNull("Hello", new Integer(3)));
+        assertFalse(Utils.isAnyNull(new ArrayList<String>(), new ArrayList<>(), new Long(-1)));
+        
+        // non-empty list with null Object()s, and other Java objects
+        assertTrue(Utils.isAnyNull((Object) null));
+        assertTrue(Utils.isAnyNull(null, new Integer(1)));
+        assertTrue(Utils.isAnyNull(null, new Random()));
+        assertTrue(Utils.isAnyNull(new Object(), null, new Object()));
+        
+        // non-empty arguments with literals
+        assertTrue(Utils.isAnyNull(null, 1, 2, 3));
+        assertTrue(Utils.isAnyNull(1, 2, 3, null));
+        assertFalse(Utils.isAnyNull(1, 2, 3));
+        assertTrue(Utils.isAnyNull(null, null, null, null));
+        
+        // another way of catching isFirstAndOnlyNull()
+        try {
+            Utils.isAnyNull(null);
+            fail("NullPointerException failed to call.");
+        } catch (NullPointerException ex) {
+            // nothing need be done here.
+        }
+    }
+
+    /**
+     * Tests the case for the method isAnyNull when the first and only argument is the Java literal 'null'
+     * 
+     * Note: DO NOT implement any other cases here.
+     * Only asserts that the method threw NullPointerException, not any particular line of code.
+     */
+    @Test(expected = NullPointerException.class)
+    public void isFirstAndOnlyNull() {
+        Utils.isAnyNull(null);
+    }
 
     @Test
     public void elementsAreUnique() throws Exception {

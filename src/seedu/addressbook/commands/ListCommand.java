c5285f1074c19157a657a180c744bcc4207e1b6d
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-09-11 16:52:23 +0800

    ListCommand.java: Migrate type of List<> allPersons

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -1,6 +1,6 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.Person;
 
 import java.util.List;
 
@@ -19,7 +19,7 @@ public class ListCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        List<Person> allPersons = addressBook.getAllPersons().immutableListView();
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }

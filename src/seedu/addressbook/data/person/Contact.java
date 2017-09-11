15d8b730a6fa50fdd3b26304124d9dac471ad9a0
Junkai <reginleiff@gmail.com>
2017-09-11 16:04:03 +0800

    Edit address class to inherit from contact class and polish up documentation

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -2,13 +2,19 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+/**
+ * Represents a Contact type in the address book (includes Phone, Email and Address).
+ * Guarantees: immutable; is valid as declared in {@link #isValid(String, String)}
+ */
 public class Contact {
-
-    public static final String MESSAGE_CONSTRAINTS = "";
-
     public final String value;
     private boolean isPrivate;
 
+    /**
+     * Validates given contact with the type of regex supplied by subclass constructor call.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
     public Contact(String value, boolean isPrivate,
                    String constraintMessage, String validationRegex) throws IllegalValueException {
         this.isPrivate = isPrivate;
@@ -19,6 +25,9 @@ public class Contact {
         this.value = trimmedContact;
     }
 
+    /**
+     * Returns true if a given string matches the input validation regex.
+     */
     public boolean isValid(String test, String validationRegex){ return test.matches(validationRegex); }
 
 

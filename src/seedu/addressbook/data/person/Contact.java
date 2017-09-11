7b763575ce31c4152779eb270c535de5183a8f74
KhorSL <stephenksl@gmail.com>
2017-09-11 21:43:30 +0800

    add Contact.java

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,53 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's contact (address, email or phone) in the address book.
+ */
+public class Contact {
+    public final String value;
+    protected boolean isPrivate;
+
+    /**
+     * Validates given contact.
+     *
+     * @throws IllegalValueException if given contact details are invalid.
+     */
+    public Contact(String contact, boolean isPrivate, String MESSAGE_CONTACT_CONSTRAINTS, String CONTACT_VALIDATION_REGEX) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedContact = contact.trim();
+        if (!isValidContact(trimmedContact, CONTACT_VALIDATION_REGEX)) {
+            throw new IllegalValueException(MESSAGE_CONTACT_CONSTRAINTS);
+        }
+        this.value = trimmedContact;
+    }
+
+    /**
+     * Returns true if the given string is a valid contact details.
+     */
+    public static boolean isValidContact(String test, String CONTACT_VALIDATION_REGEX) {
+        return test.matches(CONTACT_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

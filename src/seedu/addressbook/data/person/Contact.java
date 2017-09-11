8cda1c8264992712891012a20134dc51871f3355
Eldon\eldonng <eldonng95@gmail.com>
2017-09-11 15:50:29 +0800

    add comments for Contact class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,17 +1,22 @@
 package seedu.addressbook.data.person;
 
-import seedu.addressbook.data.exception.IllegalValueException;
 
+/**
+ * Contains contact information of Person.
+ */
 public class Contact {
 
     public String value;
     private boolean isPrivate;
 
-    public Contact(String contact, boolean isPrivate) throws IllegalValueException {
+    public Contact(String contact, boolean isPrivate) {
         this.isPrivate = isPrivate;
         value = contact;
     }
 
+    /**
+     * Returns true if the given string is a valid form or phone/email/address.
+     */
     public static boolean isValid(String test, String regex) {
        return test.matches(regex);
     }

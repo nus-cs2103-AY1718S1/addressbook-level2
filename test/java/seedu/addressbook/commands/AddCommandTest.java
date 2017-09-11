618467d746f24c06c229c7e4b7d852db659c5e7e
KhorSL <stephenksl@gmail.com>
2017-09-11 21:24:34 +0800

    Address.java: update to extend Contact, and update various jUnit test cases to import contact package.

--- test/java/seedu/addressbook/commands/AddCommandTest.java
+++ test/java/seedu/addressbook/commands/AddCommandTest.java
@@ -15,11 +15,11 @@ import org.junit.Test;
 
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.contact.Address;
+import seedu.addressbook.data.person.contact.Email;
 import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.contact.Phone;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.util.TestUtil;

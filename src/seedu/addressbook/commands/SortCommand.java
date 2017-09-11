c4e8791d5d798b0cdc478d49ef8fce6fab6e9c6e
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-09-11 21:17:56 +0800

    correct the error

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -9,7 +9,7 @@ public class SortCommand extends Command{
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts all persons in the address book by alphabetical order.\n"
+            + ": Sorts all persons in the address book in alphabetical order.\n"
             + "Example: " + COMMAND_WORD;
 
 

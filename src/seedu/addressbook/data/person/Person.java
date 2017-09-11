9a57563fd9e70f67013699ae7b1274ef1d05e9ac
jo-lyn <e0007247@u.nus.edu>
2017-09-12 01:36:08 +0800

    Add sequence number system for Person class

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -15,7 +15,11 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
+
     private final UniqueTagList tags;
+
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -25,6 +29,9 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = nextSequenceNumber;
+
+        updateNextSequenceNumber();
     }
 
     /**
@@ -52,6 +59,14 @@ public class Person implements ReadOnlyPerson {
     @Override
     public Address getAddress() {
         return address;
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    private void updateNextSequenceNumber() {
+        nextSequenceNumber++;
     }
 
     @Override

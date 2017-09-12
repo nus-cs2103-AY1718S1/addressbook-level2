0c78866033ecda0212bca1ba312209514032a3f1
Jackie Tan <free-soup@hotmail.com>
2017-09-13 00:16:08 +0800

    Updated the HelpCommand to display information about the sort function

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -11,7 +11,7 @@ public class SortCommand extends Command{
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a sorted list with index numbers.\n"
+            + ": Displays a persons in the address book as a sorted list with index numbers.\n"
             + "Example: " + COMMAND_WORD;
 
 

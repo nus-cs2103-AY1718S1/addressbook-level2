0fd5192cb8830c1223244c3de802134781e1d081
Yilun <yilun95@gmail.com>
2017-09-12 18:59:29 +0800

    Updated FindName, parser and testcases

--- src/seedu/addressbook/commands/FindNameCommand.java
+++ src/seedu/addressbook/commands/FindNameCommand.java
@@ -1,10 +1,6 @@
 package seedu.addressbook.commands;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
@@ -18,21 +14,23 @@ public class FindNameCommand extends Command{
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
 
-    private String keywords;
-    private String modifiedKeywords;
-    public FindNameCommand(Set<String> keywords){
-        this.keywords = keywords.toString().toLowerCase();
+    private Set<String> keywords;
+    public FindNameCommand(String prekeywords){
+        String[] keywordsList = prekeywords.toLowerCase().split(" ");
+        this.keywords = new HashSet<>(Arrays.asList(keywordsList));
+
     }
     public CommandResult execute() {
         final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeywordIgnoreCase(keywords);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
     }
-    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeywordIgnoreCase(String keywords) {
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeywordIgnoreCase(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
-            final String wordsInName = person.getName().getWordsInName().toString().toLowerCase();
+            String[] nameSet = person.getName().toString().toLowerCase().split(" ");
+            final Set<String> wordsInName = new HashSet<>(Arrays.asList(nameSet));
 
-            if (wordsInName.substring(1, wordsInName.length()-1).contains(keywords.substring(1, keywords.length()-1))) {
+            if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
 
             }
--- test/java/seedu/addressbook/commands/FindNameCommandTest.java
+++ test/java/seedu/addressbook/commands/FindNameCommandTest.java
@@ -23,39 +23,38 @@ public class FindNameCommandTest {
     @Test
     public void execute() throws IllegalValueException {
         //same word, same case: matched
-        assertFindNameCommandBehavior(new String[]{"Amy"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior("Amy", Arrays.asList(td.amy));
 
         //same word, different case: matched
-        assertFindNameCommandBehavior(new String[]{"aMy"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior("aMy", Arrays.asList(td.amy));
 
-        //partial word: matched
-        assertFindNameCommandBehavior(new String[]{"my"}, Arrays.asList(td.amy));
+        //partial word: not matched
+        assertFindCommandBehavior("my", Collections.emptyList());
 
         //multiple words: matched
-        assertFindNameCommandBehavior(new String[]{"Amy", "Bill", "Candy", "Destiny"},
+        assertFindCommandBehavior("amy bill candy Destiny",
                 Arrays.asList(td.amy, td.bill, td.candy));
 
         //repeated keywords: matched
-        assertFindNameCommandBehavior(new String[]{"Amy", "Amy"}, Arrays.asList(td.amy));
+        assertFindCommandBehavior("Amy Amy", Arrays.asList(td.amy));
 
         //Keyword matching a word in address: not matched
-        assertFindNameCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
+        assertFindCommandBehavior("Clementi", Collections.emptyList());
     }
 
     /**
      * Executes the find command for the given keywords and verifies
      * the result matches the persons in the expectedPersonList exactly.
      */
-    private void assertFindNameCommandBehavior(String[] keywords, List<ReadOnlyPerson> expectedPersonList) {
+    private void assertFindCommandBehavior(String keywords, List<ReadOnlyPerson> expectedPersonList) {
         FindNameCommand command = createFindNameCommand(keywords);
         CommandResult result = command.execute();
 
         assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
     }
 
-    private FindNameCommand createFindNameCommand(String[] keywords) {
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        FindNameCommand command = new FindNameCommand(keywordSet);
+    private FindNameCommand createFindNameCommand(String keywords) {
+        FindNameCommand command = new FindNameCommand(keywords);
         command.setData(addressBook, Collections.emptyList());
         return command;
     }

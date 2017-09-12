cc75d6b14a08254c0d27483fc431f3ebc6a9cd85
arturs68 <arturs6668@gmail.com>
2017-09-12 09:52:23 +0800

    extract Contact as superclass of Address Email and Phone

---
+++ src/seedu/addressbook/data/person/contact/Contact.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person.contact;
+
+/**
+ * Represents a persons contact detail
+ */
+public class Contact {
+    
+    protected boolean isPrivate;
+    
+    public Contact(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+    
+}
--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -2,11 +2,11 @@ package seedu.addressbook.util;
 
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.contact.Address;
+import seedu.addressbook.data.person.contact.Email;
 import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.contact.Phone;
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
 

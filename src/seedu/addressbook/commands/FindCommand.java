cb9bbe199913614ac3e809c50ca90c4a0513acf7
Unknown <vivekscl5@gmail.com>
2017-09-12 13:22:01 +0800

    FindCommand.java: Keywords are lower case
    Iterated through each keyword to make it lower case and assign them to an array which is then converted into a set of keywords

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -1,30 +1,37 @@
 package seedu.addressbook.commands;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
     private final Set<String> keywords;
 
+    /**
+     * Assigns the keywords(in lower case) to the attribute keyword in this class.
+     */
     public FindCommand(Set<String> keywords) {
-        this.keywords = keywords;
+        Iterator<String> iterator = keywords.iterator();
+        String[] temp = new String[keywords.size()];
+        int i = 0;
+        while (iterator.hasNext()) {
+            String curr = iterator.next();
+            temp[i] = curr.toLowerCase();
+            i++;
+        }
+        this.keywords = new HashSet<>(Arrays.asList(temp));
     }
 
     /**

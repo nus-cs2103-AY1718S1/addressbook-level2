6e57010bdfc7726e8a83e52d54497f9c6a11ba2c
Daniel Chew <cctdaniel@outlook.com>
2017-09-12 19:50:30 +0800

    Extracted commonalities from Address.java, Email.java and Phone.java into the parent class Contact.java

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,4 +1,31 @@
 package seedu.addressbook.data.person;
 
 public class Contact {
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Contact() {
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Phone // instanceof handles nulls
+                && this.value.equals(((Phone) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }

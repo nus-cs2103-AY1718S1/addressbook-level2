6d911dd27d2355a15c2f641a05c895ab0e010154
KEVIN\Kevin <kevin2leona@gmail.com>
2017-09-13 02:47:43 +0800

    Create Contact class to represent a Person's contact details

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a Person's contact details in the addressbook, which includes phone, email, and address
+ * 
+ */
+public class Contact {
+}

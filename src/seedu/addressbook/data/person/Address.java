2e43bb80d28d24be9fa00309d7769f83b2f9fe00
Xiang Hailin <895227442@qq.com>
2017-09-12 16:42:09 +0800

    Revert "finish LO-Inheritance"
    This reverts commit 50dec5a2ad6206f36f4d122632b3fcc5d8bd8680.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,11 +6,14 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address extends Contact {
+public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+
+    public final String value;
+    private boolean isPrivate;
 
     /**
      * Validates given address.
@@ -33,4 +36,24 @@ public class Address extends Contact {
         return test.matches(ADDRESS_VALIDATION_REGEX);
     }
 
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Address // instanceof handles nulls
+                && this.value.equals(((Address) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }

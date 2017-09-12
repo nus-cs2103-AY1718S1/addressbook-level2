0bae218f2152ffca49c5765933fa86ae3a79ca74
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2017-09-12 11:28:11 +0800

    UtilsTest.java: remove unnecessary import

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,7 +3,6 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 

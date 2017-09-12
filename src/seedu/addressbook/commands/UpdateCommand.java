b70937e8a9fc99e618b9f4f2e4126e74ee16acef
dalessr <dale.sunshengran@gmail.com>
2017-09-12 10:46:59 +0800

    Fix bug: change inconsistant outputs

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -15,13 +15,13 @@ public class UpdateCommand extends Command {
 
     public static final String COMMAND_WORD = "update";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates a person's info in the address book. "
             + "Contact details can be marked private by prepending 'p' to the prefix.\n"
             + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " John Doe p/12345678 e/johnd@gmail.com a/311, Clementi Ave 2, #01-31 t/friends t/owesMoney";
+            + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
-    public static final String MESSAGE_SUCCESS = "Existing person updated: %1$s";
+    public static final String MESSAGE_SUCCESS = "person info updated: %1$s";
     public static final String MESSAGE_NOT_FOUND_PERSON = "This person does not exist in the address book";
 
     private final Person toUpdate;

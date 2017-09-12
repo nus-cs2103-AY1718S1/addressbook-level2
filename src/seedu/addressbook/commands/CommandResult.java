fda437609d2dcc4f2f2b05b82227fec295b13938
arturs68 <arturs6668@gmail.com>
2017-09-12 16:00:00 +0800

    add displaying an updated list of all the persons after the execution of add and delete commands

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -12,6 +12,9 @@ public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     public final String feedbackToUser;
+    
+    /** The feedback message to be shown to the user after the list of persons. Contains a description of the execution result */
+    private String relevantPersonsFeedback = null;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -25,6 +28,12 @@ public class CommandResult {
         this.feedbackToUser = feedbackToUser;
         this.relevantPersons = relevantPersons;
     }
+    
+    public CommandResult(String feedbackToUser, List<? extends ReadOnlyPerson> relevantPersons, String relevantPersonsFeedback) {
+        this.feedbackToUser = feedbackToUser;
+        this.relevantPersons = relevantPersons;
+        this.relevantPersonsFeedback = relevantPersonsFeedback;
+    }
 
     /**
      * Returns a list of persons relevant to the command command result, if any.
@@ -32,5 +41,11 @@ public class CommandResult {
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
         return Optional.ofNullable(relevantPersons);
     }
-
+    
+    /**
+     * Returns a secondary feedback relevant to the list of persons, if any.
+     */
+    public Optional<String> getRelevantPersonsFeedback() {
+        return Optional.ofNullable(relevantPersonsFeedback);   
+    }
 }

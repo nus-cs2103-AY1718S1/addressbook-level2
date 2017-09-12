c25bfbc40072a59b524d75d87c5504638458d6db
Zames <zames.chua@gmail.com>
2017-09-13 00:52:45 +0800

    Remove unused imports in JUnit tests for EditCommandTest

--- test/java/seedu/addressbook/commands/EditCommandTest.java
+++ test/java/seedu/addressbook/commands/EditCommandTest.java
@@ -1,27 +1,15 @@
 package seedu.addressbook.commands;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
 
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
 import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.util.TestUtil;
 
 public class EditCommandTest {

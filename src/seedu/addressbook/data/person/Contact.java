a1928211fb82802b0748ae3a5a4ac843de5e1a27
itsdickson <itsklein@hotmail.com>
2017-09-12 12:24:35 +0800

    Contact.java: added common variables and methods from Address.java, Email.java, Phone.java into superclass

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,4 +1,34 @@
 package seedu.addressbook.data.person;
 
 public class Contact {
+    protected String value;
+    private boolean isPrivate;
+
+    protected Contact() {
+    }
+
+    public Contact(String contact, boolean isPrivate) {
+        this.value = contact.trim();
+        this.isPrivate = isPrivate;
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate(){
+        return isPrivate;
+    }
 }

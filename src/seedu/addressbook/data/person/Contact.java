df7d3a0460b04d85482997b51392bad868c8b0fa
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2017-09-12 18:03:47 +0800

    Contact.java: Add more methods

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,25 +1,51 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Super class for Phone, Email and Address
+ */
+
 public class Contact {
 
-	protected String EXAMPLE;
-	protected String MESSAGE_CONTACT_CONSTRAINTS;
-	protected String CONTACT_VALIDATION_REGEX;
+	public final String EXAMPLE;
+	public final String MESSAGE_CONTACT_CONSTRAINTS;
+	public final String CONTACT_VALIDATION_REGEX;
 
-	public String value;
+	public final String value;
 	private boolean isPrivate;
 
 	/**
-	 * Returns true if the given string is a valid person phone number.
+	 *  Constructor for setting the values and validating
+	 *
+	 *  @throws IllegalValueException if given value string is invalid.
 	 */
-	public boolean isValidContact(String test) {
-		return test.matches(this.CONTACT_VALIDATION_REGEX);
+	public Contact(String example,
+				   String constraints,
+				   String regex,
+				   String value,
+				   boolean isPrivate) throws IllegalValueException {
+		
+		this.EXAMPLE = example;
+		this.MESSAGE_CONTACT_CONSTRAINTS = constraints;
+		this.CONTACT_VALIDATION_REGEX = regex;
+
+		this.setPrivacy(isPrivate);
+		String trimmedValue = value.trim();
+		if (!isValidContact(trimmedValue)) {
+			throw new IllegalValueException(MESSAGE_CONTACT_CONSTRAINTS);
+		}
+		this.value = trimmedValue;
 	}
 
+	public boolean isPrivate() { return this.isPrivate; }
+
+	private void setPrivacy(boolean privacy){ this.isPrivate = privacy; }
+
+	public boolean isValidContact(String test) { return test.matches(CONTACT_VALIDATION_REGEX); }
+
 	@Override
-	public String toString() {
-		return value;
-	}
+	public String toString() { return value; }
 
 	@Override
 	public boolean equals(Object other) {
@@ -28,13 +54,8 @@ public class Contact {
 				&& this.value.equals(((Phone) other).value)); // state check
 	}
 
-	@Override
-	public int hashCode() {
-		return value.hashCode();
-	}
 
-	public boolean isPrivate() {
-		return isPrivate;
-	}
+	@Override
+	public int hashCode() { return value.hashCode(); }
 
 }

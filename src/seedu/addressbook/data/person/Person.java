3ff5ffbd922b0849322d23edf03de31fed0ba7a1
KEVIN\Kevin <kevin2leona@gmail.com>
2017-09-13 03:14:50 +0800

    Change Person class to store sequenceNumber and increment nextSequenceNumber for every person made

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -23,6 +23,8 @@ public class Person implements ReadOnlyPerson {
      * Assumption: Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
+        this.sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber+= 1;
         this.name = name;
         this.phone = phone;
         this.email = email;

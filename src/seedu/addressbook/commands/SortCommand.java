5bdd66085635e7f537dac4a4485fd836827f3607
cjianhui <31098608+cjianhui@users.noreply.github.com>
2017-09-13 00:25:28 +0800

    Add SortCommand.java

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -7,14 +7,17 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
  * Sorts all persons in address book according to one of the following arguments - name, email, phone, block
  * If no arguments are specified, persons will be sorted by name
  */
+
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
+
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts persons according to field specified (name if none specified).  "
             + "Parameters: [FIELD] (name/phone/email/address)\n"
             + "Example: " + COMMAND_WORD + " address";
 
+    public static final String MESSAGE_SUCCESS = "AddressBook sorted by %1$s";
     private final String field;
 
     public SortCommand(String field) {
@@ -23,8 +26,8 @@ public class SortCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        addressBook.sortByField(field);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, field));
     }
 
 }

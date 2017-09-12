67e10cd5bd92a0cb5e627464459fb42b8187c890
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2017-09-12 21:08:49 +0800

    Update find to accept tag as argument tags must have t/ prefix

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -7,9 +7,10 @@ import java.util.List;
 import java.util.Set;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
-
+import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.data.tag.Tag;
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all persons in address book whose name or tag contains any of the argument keywords.
  * Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
@@ -17,44 +18,47 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "the specified keywords (case-sensitive) or contains tag(s) specified and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS] t/TAG_NAME \n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie t/tag";
 
-    private final Set<String> keywords;
+    private final Set<String> nameKeywords;
+    private final Set<String> tagKeywords;
 
-    public FindCommand(Set<String> keywords) {
-        this.keywords = keywords;
+    public FindCommand(Set<String> nameKeywords, Set<String> tagKeywords) {
+        this.nameKeywords = nameKeywords;
+        this.tagKeywords = tagKeywords;
     }
 
     /**
      * Returns a copy of keywords in this command.
      */
     public Set<String> getKeywords() {
-        return new HashSet<>(keywords);
+        return new HashSet<>(nameKeywords);
     }
 
     @Override
     public CommandResult execute() {
-        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(nameKeywords, tagKeywords);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
     }
 
     /**
      * Retrieves all persons in the address book whose names contain some of the specified keywords.
      *
-     * @param keywords for searching
+     * @param nameKeywords for searching
      * @return list of persons found
      */
-    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> nameKeywords, Set<String> tagKeywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             
             boolean isPersonAdded = false;
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            
-            for(String s : keywords) {
+            final UniqueTagList personTags = person.getTags();
+            //check for matching name keyword
+            for(String s : nameKeywords) {
                 for(String w : wordsInName) {
                     if (w.startsWith(s)) {
                         if(!isPersonAdded) {
@@ -64,6 +68,21 @@ public class FindCommand extends Command {
                     }                       
                 }
             }
+            
+            //check for tag if person has not been added
+            if(!isPersonAdded) {
+                for(String tag : tagKeywords) {
+                    for(Tag personTag : personTags) {
+                        if(personTag.tagName.equals(tag)) {
+                            if(!isPersonAdded) {
+                                matchedPersons.add(person);
+                                isPersonAdded = true;
+                            }
+                        }
+                    }
+                }
+            }
+            
         }
         
         return matchedPersons;

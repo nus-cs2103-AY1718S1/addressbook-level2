0896c47500881d33f87007c896ea544f79ebb3c2
Siddharth Madhavan <siddharth.madhavan@gmail.com>
2017-09-12 18:56:01 +0800

    added isAnyNull test case

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,11 +1,14 @@
 package seedu.addressbook.common;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class UtilsTest {
 

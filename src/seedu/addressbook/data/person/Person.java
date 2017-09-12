53f541639e7ca2dad1582fd8754073dd78e91c4f
Jelena <neohuilingjelena@gmail.com>
2017-09-12 21:27:37 +0800

    Person.java: add sequence number to indicate order of Person object creation

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -16,6 +16,9 @@ public class Person implements ReadOnlyPerson {
     private Address address;
 
     private final UniqueTagList tags;
+
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
@@ -32,6 +37,8 @@ public class Person implements ReadOnlyPerson {
      */
     public Person(ReadOnlyPerson source) {
         this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+        sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     @Override

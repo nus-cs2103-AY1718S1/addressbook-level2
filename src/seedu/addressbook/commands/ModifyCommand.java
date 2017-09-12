716514fb0d9ad377b78e5830f7f11e62889c9ee2
DESKTOP-PG4SGTP\Celia <huwanqing1998@gmail.com>
2017-09-12 12:05:59 +0800

    added modify command

--- src/seedu/addressbook/commands/ModifyCommand.java
+++ src/seedu/addressbook/commands/ModifyCommand.java
@@ -3,29 +3,29 @@ package seedu.addressbook.commands;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.*;
 
-public class ModifyCommand extends Command {
+public class ModifyCommand extends Command{
     public static final String COMMAND_WORD="change";
-    private String[] args;
+    private String args;
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Change the PHONE, EMAIL OR ADDRESS of the person identified by the index number used in the last person listing.\n"
             + "Parameters: INDEX and any of p/PHONE e/EMAIL a/ADDRESS\n"
-            + "Example: " + COMMAND_WORD + " 1"+ "e/EMAIL \n"
+            + "Example: " + COMMAND_WORD + " 1"+ " e/EMAIL \n"
             + "Only one change available each time";
 
-    public static final String MESSAGE_CHANGE_SUCCESSFUL= "change successful";
 
-    public ModifyCommand(int TargetVisibleIndex, String[] args){
+    public ModifyCommand(int TargetVisibleIndex, String args){
         super(TargetVisibleIndex);
         this.args=args;
     }
 
     @Override
-    public CommandResult execute() throws UniquePersonList.PersonNotFoundException {
-        final ReadOnlyPerson target = getTargetPerson();
-        addressBook.removePerson(target);
-        String ops=args[2].substring(0,2);
-        String info=args[2].substring(2);
-        try {
+    public CommandResult execute()  {
+        try{
+            final ReadOnlyPerson target = getTargetPerson();
+            addressBook.removePerson(target);
+            String ops=args.substring(0,2);
+            String info=args.substring(2);
+
             switch (ops) {
                 case "p/":
                     Phone newPhone = new Phone(info, target.getPhone().isPrivate());
@@ -42,8 +42,12 @@ public class ModifyCommand extends Command {
                 default:
                     return new CommandResult("Invalid input message, cannot change the desired person's information");
             }
-        }catch(IllegalValueException e){
-            e.printStackTrace();
+        } catch (UniquePersonList.PersonNotFoundException e) {
+            return new CommandResult("Person not found!");
+        } catch(IllegalValueException e){
+            return new CommandResult("Illegal value!");
+        }catch (IndexOutOfBoundsException e){
+            return new CommandResult("Invalid index, please enter valid index number!");
         }
         return new CommandResult("the information has been successfully changed!");
     }

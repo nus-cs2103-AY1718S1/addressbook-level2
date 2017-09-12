d78050efb7c2676df5a5451b8a3212851f96474f
Pj Poh <e0037036@u.nus.edu>
2017-09-12 21:09:53 +0800

    Extract out commonalities to form a superclass Contact.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * A superclass for Phone, Email, and Java. We seek to extract out the common methods.
+ */
+
+public class Contact {
+    public String value;
+    private boolean isPrivate;
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Address // instanceof handles nulls
+                && this.value.equals(((Address) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

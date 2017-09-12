5c52e5830f7bcbf06206654b785efcbce2637709
hymss <hyma.tiyyagura@gmail.com>
2017-09-13 01:32:02 +0800

    debugging sortcommand

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -19,7 +19,7 @@ public class SortCommand extends Command{
 
     @Override
     public CommandResult execute(){
-        List <ReadOnlyPerson> allPersons = getAllPersonsSorted().immutableListView();
+        List <ReadOnlyPerson> allPersons = addressBook.getAllPersonsSorted().immutableListView();
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }

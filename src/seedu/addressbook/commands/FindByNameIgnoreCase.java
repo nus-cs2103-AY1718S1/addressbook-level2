50e37f7384625491c20032265a8035c2061d8b3f
Yilun <yilun95@gmail.com>
2017-09-12 16:59:58 +0800

    Added new command FindByIgnoreName Added corresponding parser

---
+++ src/seedu/addressbook/commands/FindByNameIgnoreCase.java
@@ -0,0 +1,44 @@
+package seedu.addressbook.commands;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+public class FindByNameIgnoreCase extends Command{
+    public static final String COMMAND_WORD = "findName";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+
+    private String keywords;
+    public FindByNameIgnoreCase(String keywords){
+        this.keywords = keywords;
+    }
+    public CommandResult execute() {
+        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeywordIgnoreCase(keywords);
+        return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
+    }
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeywordIgnoreCase(String keywords) {
+        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : addressBook.getAllPersons()) {
+            final String wordsInName = person.getName().getWordsInName().toString().toLowerCase();
+
+            if (wordsInName.substring(1, wordsInName.length()-1).contains(keywords.substring(1, keywords.length()-1))) {
+                matchedPersons.add(person);
+
+            }
+        }
+        return matchedPersons;
+    }
+
+
+
+}

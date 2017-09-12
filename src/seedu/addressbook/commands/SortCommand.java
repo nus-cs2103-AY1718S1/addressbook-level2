34bda08092ea1bce38bbddf092d93cfff60deb57
Icehawk <e0015833@u.nus.edu>
2017-09-13 03:45:30 +0800

    Add SortCommand.java

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,12 @@
+package seedu.addressbook.commands;
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all entries in address book. "
+            + "Entries will be listed in alphabetical order of first names. \n"
+            + "Example: " + COMMAND_WORD;
+
+    
+}

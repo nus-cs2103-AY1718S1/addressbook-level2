e08bcdc2a94f4eb2e31b75256778ffa5b0d478dc
itsdickson <itsklein@hotmail.com>
2017-09-13 00:49:05 +0800

    EditCommand.java: added main code along with execute method

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,5 +1,15 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.ui.TextUi;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class EditCommand extends Command{
     public static final String COMMAND_WORD = "edit";
 
@@ -7,10 +17,42 @@ public class EditCommand extends Command{
             + ": Edits a person's details in the address book based on the index of last viewed list"
             + " and the detail that the user wants to edit.\n"
             + "Example: " + COMMAND_WORD + " 1\n"
-            + " e/jdoe95@gmail.com";
+            + "John Dog p/98654321 e/jdog@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+
+    private final Person toEdit;
+
+    public EditCommand(int targetIndex, String name,
+                      String phone, boolean isPhonePrivate,
+                      String email, boolean isEmailPrivate,
+                      String address, boolean isAddressPrivate,
+                      Set<String> tags) throws IllegalValueException {
+        super(targetIndex);
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toEdit = new Person(
+                new Name(name),
+                new Phone(phone, isPhonePrivate),
+                new Email(email, isEmailPrivate),
+                new Address(address, isAddressPrivate),
+                new UniqueTagList(tagSet)
+        );
+    }
 
     @Override
     public CommandResult execute() {
-        
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            addressBook.editPerson(getTargetIndex(), target, toEdit);
+            return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, target));
+
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (UniquePersonList.PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        }
     }
 }

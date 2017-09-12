afc945017e190dd7aa72215804c70d1dae89372a
itsdickson <itsklein@hotmail.com>
2017-09-12 12:43:11 +0800

    EditCommand.java: added COMMAND_WORD and MESSAGE_USAGE

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,5 +1,16 @@
 package seedu.addressbook.commands;
 
 public class EditCommand extends Command{
-    
+    public static final String COMMAND_WORD = "edit";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits a person's details in the address book based on the index of last viewed list"
+            + " and the detail that the user wants to edit.\n"
+            + "Example: " + COMMAND_WORD + " 1\n"
+            + " e/jdoe95@gmail.com";
+
+    @Override
+    public CommandResult execute() {
+        
+    }
 }

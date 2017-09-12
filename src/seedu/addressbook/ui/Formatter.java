9c46a245114b244a02f3220df256cef195e7f821
wenzongteo <wenzongteo@gmail.com>
2017-09-12 20:04:42 +0800

    [W5.10][W14-A1]Teo Wen Zong
    Add Formatter.java to remove formatting jobs from TestUI.java

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,55 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+public class Formatter {
+    private static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    public String getDivider(){
+        return DIVIDER;
+    }
+
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+    public String generateMessage(String... message) {
+        StringBuilder sb = new StringBuilder();
+        for (String m : message) {
+            sb.append(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX)+"\n");
+        }
+        return sb.toString();
+    }
+
+    public void printEnterCommand(){
+        System.out.print(LINE_PREFIX + "Enter command: ");
+    }
+
+}

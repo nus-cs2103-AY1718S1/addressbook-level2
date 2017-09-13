5e3cc0be9eaf03f7e12835c01cf52147fdb930ea
Royce <e0030923@u.nus.edu>
2017-09-13 20:21:25 +0800

    Revert "update UtilsTest.java"
    This reverts commit 38e00e959751f4fda44a836ab7736d6543274e3a.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -36,27 +36,6 @@ public class UtilsTest {
         assertNotUnique(null, "a", "b", null);
     }
 
-    @Test
-    public void isAnyNull() throws Exception{
-        // empty list
-        assertNoNull();
-
-        // no nulls
-        assertNoNull(1);
-        assertNoNull(1, 2, 3, 4);
-        assertNoNull("abc");
-        assertNoNull("abc", "def", "ghi");
-
-        // have nulls
-        assertHaveNull(1, null);
-        assertHaveNull(1, null, 2, 3, 4);
-        assertHaveNull(1, null, 2, null, null);
-        assertHaveNull(null, null, null, null);
-    }
-    private void assertHaveNull(Object... objects) { assertTrue(Utils.isAnyNull(objects)); }
-
-    private void assertNoNull(Object... objects) { assertFalse(Utils.isAnyNull(objects)); }
-
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }

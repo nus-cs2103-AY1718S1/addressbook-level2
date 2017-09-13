7cacfbac42221e23ac577ffcfcd5600225ac6518
LYS <honestkiddy@gmail.com>
2017-09-14 00:06:29 +0800

    Formatter.java: Format variables were extracted into this class to aid in future format changes

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.ui;
+
+public class Formatter {
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    protected static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    protected static final String LS = System.lineSeparator();
+
+    protected static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    protected static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+}

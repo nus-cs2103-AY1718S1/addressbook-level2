e188515dd03a2f9f311da638f3dd80457088c201
DESKTOP-JENMVTR\User <tby1994>
2017-09-13 11:06:58 +0800

    Added new comment to PersonComparator

--- src/seedu/addressbook/data/person/PersonComparator.java
+++ src/seedu/addressbook/data/person/PersonComparator.java
@@ -2,6 +2,9 @@ package seedu.addressbook.data.person;
 
 import java.util.Comparator;
 
+/**
+ * Compare the names of 2 persons alphabetically
+ */
 public class PersonComparator implements Comparator<Person>{
     @Override
     public int compare(Person o1, Person o2) {

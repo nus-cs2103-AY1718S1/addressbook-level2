767784086a795a5f170debe37e98f3457dbceafd
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-09-14 00:47:56 +0800

    SortCommand.java : change COMMAND_WORD and MESSAGE_USAGE

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -8,12 +8,12 @@ import java.util.List;
 /**
  * Lists all persons in the address book to the user.
  */
-public class ListCommand extends Command {
+public class SortCommand extends Command {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Sorts and displays all persons in the address book as a list with index numbers.\n"
             + "Example: " + COMMAND_WORD;
 
 

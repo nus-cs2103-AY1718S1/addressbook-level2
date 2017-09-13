180219a1c8a6653cb13526c46c90d74f856ef280
Dong Shaocong <shaocong.dong@u.nus.edu>
2017-09-13 15:27:22 +0800

    Modify the edit command return message
    Change to"1 person edited as long as the edit is successful.

--- src/seedu/addressbook/common/Messages.java
+++ src/seedu/addressbook/common/Messages.java
@@ -12,7 +12,7 @@ public class Messages {
     public static final String MESSAGE_PERSON_NOT_IN_ADDRESSBOOK = "Person could not be found in address book";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_PERSONS_SORTED_OVERVIEW = "%1$d persons sorted!";
-    public static final String MESSAGE_PERSONS_EDITED_OVERVIEW = "%1$d persons edited!";
+    public static final String MESSAGE_PERSONS_EDITED_OVERVIEW = "1 person edited!";
     public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: " +
             "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
     public static final String MESSAGE_WELCOME = "Welcome to your Address Book!";

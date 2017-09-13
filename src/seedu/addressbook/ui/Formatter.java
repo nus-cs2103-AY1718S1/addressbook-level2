f2da97472bfc94b156b5e9367b58f674417b2832
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2017-09-13 16:20:55 +0800

    Formatter.java: Add missing parts

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 public class Formatter {
 
+
+	public Formatter(){}
+
 	/** Offset required to convert between 1-indexing and 0-indexing.  */
 	public static final int DISPLAYED_INDEX_OFFSET = 1;
 
+	/** Format of indexed list item */
+	private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
 	/** Formats a list of strings as a viewable indexed list. */
-	private static String getIndexedListForViewing(List<String> listItems) {
+	public static String getIndexedListForViewing(List<String> listItems) {
 		final StringBuilder formatted = new StringBuilder();
 		int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
 		for (String listItem : listItems) {

6059bbd36b7a0e854ccc9641fe3c5b2f97c9684c
aali195 <a.ali15@liverpool.ac.uk>
2017-09-13 13:28:55 +0800

    implement the random person feature code

--- src/seedu/addressbook/commands/RandomCommand.java
+++ src/seedu/addressbook/commands/RandomCommand.java
@@ -2,7 +2,9 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 /**
@@ -16,11 +18,26 @@ public class RandomCommand extends Command {
             + ": Returns a random person from the address book.\n"
             + "Example: " + COMMAND_WORD;
 
-    //TO DO, implement the random function
     @Override
     public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        if (allPersons.isEmpty()) {
+            return new CommandResult(getMessageForAddressBookIsEmpty());
+        } else {
+            final List<ReadOnlyPerson> personFound = getRandomPerson(allPersons);
+            return new CommandResult(getMessageForRandomPersonFound(), personFound);
+        }
     }
 
+    /**
+     * Retrieves all persons in the address book and returns a random person from the list.
+     *
+     * @param allPersons all the persons in the address book
+     * @return random person
+     */
+    private List<ReadOnlyPerson> getRandomPerson(List<ReadOnlyPerson> allPersons) {
+        final List<ReadOnlyPerson> randomPerson = new ArrayList<>();
+        randomPerson.add(allPersons.get(new Random().nextInt(allPersons.size())));
+        return randomPerson;
+    }
 }

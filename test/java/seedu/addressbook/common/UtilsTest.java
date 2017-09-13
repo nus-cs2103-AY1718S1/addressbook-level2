af25c9cb9a5aa897d55ab29bdb47042781c75e7a
Royce <e0030923@u.nus.edu>
2017-09-13 11:41:31 +0800

    add UtilsTest.java

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -62,6 +62,29 @@ public class UtilsTest {
         assertNotUnique(null, "a", "b", null);
     }
 
+    @Test
+    public void isAnyNull() throws Exception {
+        //empty list
+        assertWithoutNull();
+
+        //with nulls
+        assertWithNull(null);
+        assertWithNull("abc", null);
+        assertWithNull(1, 2, null, 3);
+        assertWithNull(null, null, null, null);
+
+        //without null
+        assertWithoutNull("abc");
+        assertWithoutNull(1, 2, 3, 4);
+        assertWithoutNull("abc", "def", 123);
+        assertWithoutNull(123, "0", "abc", 456);
+
+    }
+
+    public void assertWithNull(Object... objects) { assertTrue(Utils.isAnyNull(objects)); }
+
+    public void assertWithoutNull(Object... objects) { assertFalse(Utils.isAnyNull(objects)); }
+
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }

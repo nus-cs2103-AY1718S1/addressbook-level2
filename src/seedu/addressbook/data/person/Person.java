bbc4bc2a552a3f66acf0edef09df3299ce835072
chernghann <chernghann.chia@gmail.com>
2017-09-13 23:42:34 +0800

    Convert parseCommand to static method and static sequence number

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -15,6 +15,9 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private int sequenceNumber;
+    public static int nextSequenceNumber=1;
+
     private final UniqueTagList tags;
     /**
      * Assumption: Every field must be present and not null.
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+
+        sequenceNumber = nextSequenceNumber++;
     }
 
     /**

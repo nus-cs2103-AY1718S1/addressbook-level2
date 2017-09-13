9c6b962d59f41e5b2d35809be242d63565fe3d45
Karrui Lau <karrui.lau@gmail.com>
2017-09-13 11:08:29 +0800

    Add Contact class
    Extract commonalities from Phone, Email and Address classes into a parent class called Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,32 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+    public final String value;
+    private boolean isPrivate;
+
+    public Contact(String value, boolean isPrivate) {
+        this.value = value;
+        this.isPrivate = isPrivate;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Address // instanceof handles nulls
+                && this.value.equals(((Address) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

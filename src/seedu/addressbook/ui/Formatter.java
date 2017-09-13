b34eb09fbb5c91166b8d3cde58b391e97d3aa5e7
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2017-09-13 16:01:25 +0800

    Formatter.java: Create new class and add formatting functions to the class

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,32 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+public class Formatter {
+
+	/** Offset required to convert between 1-indexing and 0-indexing.  */
+	public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+	/** Formats a list of strings as a viewable indexed list. */
+	private static String getIndexedListForViewing(List<String> listItems) {
+		final StringBuilder formatted = new StringBuilder();
+		int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+		for (String listItem : listItems) {
+			formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+			displayIndex++;
+		}
+		return formatted.toString();
+	}
+
+	/**
+	 * Formats a string as a viewable indexed list item.
+	 *
+	 * @param visibleIndex visible index for this listing
+	 */
+	private static String getIndexedListItem(int visibleIndex, String listItem) {
+		return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+	}
+
+
+
+}

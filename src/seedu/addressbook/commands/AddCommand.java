9e22868b32519c4d94df4f78028ee8c4d50837b1
Nelson Quek <nelsonquek@Nelsons-MacBook-Pro.local>
2017-09-14 02:03:30 +0800

    Corrected typo in Parameters

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -24,7 +24,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Contact details can be marked private by prepending 'p' to the prefix.\n"
-            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [f]f/FACULTY [p]a/ADDRESS  [t/TAG]...\n"
+            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]f/FACULTY [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " John Doe p/98765432 e/johnd@gmail.com f/computing a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 

e2e6fa6d3758bf59db354326db64c4200f194c5d
Deric Khoo Jia Wei <e0031526@u.nus.edu>
2017-09-14 01:20:02 +0800

    SortCommand.java : create method sortByName

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,24 +2,34 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
-import java.util.List;
-
+import java.util.*;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all persons in the address book to the user in alphabetical order.
  */
-public class SortCommand extends Command {
 
+public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts and displays all persons in the address book as a list with index numbers.\n"
+            + ": Sort and display the contact list in alphabetical order\n"
             + "Example: " + COMMAND_WORD;
-
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        List<ReadOnlyPerson> sortedList = sortByName(addressBook.getAllPersons().immutableListView());
+        return new CommandResult(getMessageForPersonListShownSummary(sortedList), sortedList);
+    }
+
+    private List<ReadOnlyPerson> sortByName(List<ReadOnlyPerson> dummyList) {
+        NameComparator nameComparator = new NameComparator();
+        List<ReadOnlyPerson> sortedContacts = new ArrayList<>();
+        for (ReadOnlyPerson person: addressBook.getAllPersons()) {
+            sortedContacts.add(person);
+
+        }
+        Collections.sort(sortedContacts, nameComparator);
+
+        return sortedContacts;
     }
 }

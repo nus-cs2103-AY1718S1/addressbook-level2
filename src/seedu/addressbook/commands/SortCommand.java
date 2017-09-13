81dde96bd69329c873fe64e8e3d4a22a2afb04d6
Icehawk <e0015833@u.nus.edu>
2017-09-14 01:00:36 +0800

    Fix SortCommand.java

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -8,18 +8,24 @@ import java.util.List;
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_SUCCESS = "Sort complete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all entries in address book. "
             + "Entries will be listed in alphabetical order of first names. \n"
             + "Example: " + COMMAND_WORD;
 
     @Override
-    public CommandResult execute() {
+    /*public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
         List<ReadOnlyPerson> mutableList = allPersons;
         Collections.sort(mutableList);
 
-        return new CommandResult(getMessageForPersonListShownSummary(mutableList), mutableList);
-
+        return new CommandResult(MESSAGE_SUCCESS);
+        //getMessageForPersonListShownSummary(mutableList)
+    }
+    */
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }

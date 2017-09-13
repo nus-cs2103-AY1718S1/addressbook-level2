e1170c597bd6c0bcf9ff193954819c475a8931d4
shunyong <e0002695@u.nus.edu>
2017-09-13 08:53:07 +0800

    Implement methods in Contact.java

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,4 +1,27 @@
 package seedu.addressbook.data.person;
 
 public class Contact {
+
+
+    public final String value;
+    private boolean isPrivate;
+
+
+    public Contact(String value, boolean isPrivate){
+        this.value = value;
+        this.isPrivate = isPrivate;
+    }
+
+
+    public String toString(){
+        return value;
+    }
+
+    public boolean isPrivate(){
+        return isPrivate;
+    }
+
+    public int hashCode(){
+        return value.hashCode();
+    }
 }

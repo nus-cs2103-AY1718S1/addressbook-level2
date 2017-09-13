27d6c7a54f70711439ee06812bf3825006bd7d34
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-09-13 14:21:10 +0800

    Added dummy test

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class SortCommandTest {
 
     @Test
-    private void sortCommand_successMessage() throws Exception {
+    public void sortCommand_successMessage() throws Exception {
     }
 
 }

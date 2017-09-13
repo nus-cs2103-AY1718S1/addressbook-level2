08fa52bbd30bea6b723c15b81f152309b197e437
Chua Wei Wen <weiwen.chua5@gmail.com>
2017-09-13 15:21:29 +0800

    Updated UtilsTest.java

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -9,6 +9,32 @@ import java.util.List;
 import org.junit.Test;
 
 public class UtilsTest {
+
+    @Test
+    public void elementsAreUnique() throws Exception {
+        // empty list
+        assertAreUnique();
+
+        // only one object
+        assertAreUnique((Object) null);
+        assertAreUnique(1);
+        assertAreUnique("");
+        assertAreUnique("abc");
+
+        // all objects unique
+        assertAreUnique("abc", "ab", "a");
+        assertAreUnique(1, 2);
+
+        // some identical objects
+        assertNotUnique("abc", "abc");
+        assertNotUnique("abc", "", "abc", "ABC");
+        assertNotUnique("", "abc", "a", "abc");
+        assertNotUnique(1, new Integer(1));
+        assertNotUnique(null, 1, new Integer(1));
+        assertNotUnique(null, null);
+        assertNotUnique(null, "a", "b", null);
+    }
+
     @Test
     public void isAnyNull() {
         // empty list
@@ -39,31 +65,6 @@ public class UtilsTest {
         assertFalse(Utils.isAnyNull(nullList));
     }
 
-    @Test
-    public void elementsAreUnique() throws Exception {
-        // empty list
-        assertAreUnique();
-
-        // only one object
-        assertAreUnique((Object) null);
-        assertAreUnique(1);
-        assertAreUnique("");
-        assertAreUnique("abc");
-
-        // all objects unique
-        assertAreUnique("abc", "ab", "a");
-        assertAreUnique(1, 2);
-
-        // some identical objects
-        assertNotUnique("abc", "abc");
-        assertNotUnique("abc", "", "abc", "ABC");
-        assertNotUnique("", "abc", "a", "abc");
-        assertNotUnique(1, new Integer(1));
-        assertNotUnique(null, 1, new Integer(1));
-        assertNotUnique(null, null);
-        assertNotUnique(null, "a", "b", null);
-    }
-
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
@@ -71,4 +72,5 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+
 }

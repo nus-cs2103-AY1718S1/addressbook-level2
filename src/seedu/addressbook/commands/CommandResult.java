6a1fe60fccd958112f2160841152192055252c89
Arnold <arnol.ice@gmail.com>
2017-09-14 02:52:44 +0800

    More W4.3 LO edits removal

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -12,8 +12,6 @@ public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     public final String feedbackToUser;
-    //Arnol changed feedbackToUser to private
-    //But Arnol changed it back
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;

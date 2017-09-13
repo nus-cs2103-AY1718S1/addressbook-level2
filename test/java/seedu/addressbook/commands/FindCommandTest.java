28349bf43c6b7ec4baeb186d5c2ebb9f90326e9e
Zhang Shuoyang <zhangshuoyang@Zhangs-MacBook-Pro-3.local>
2017-09-13 13:35:41 +0800

    Changed test case

--- test/java/seedu/addressbook/commands/FindCommandTest.java
+++ test/java/seedu/addressbook/commands/FindCommandTest.java
@@ -26,7 +26,8 @@ public class FindCommandTest {
         assertFindCommandBehavior(new String[]{"Amy"}, Arrays.asList(td.amy));
 
         //same word, different case: not matched
-        assertFindCommandBehavior(new String[]{"aMy"}, Collections.emptyList());
+        assertFindCommandBehavior(new String[]{"aMy"}, Arrays.asList(td.amy));
+//        assertFindCommandBehavior(new String[]{"aMy"}, Collections.emptyList());
 
         //partial word: not matched
         assertFindCommandBehavior(new String[]{"my"}, Collections.emptyList());

e2ff1a08525e8dc872db6a1ec2bc8aad599690f3
DESKTOP-7SUIEBU\Jasmine <jgreenteaj@gmail.com>
2017-09-13 10:12:33 +0800

    update tests and changed Address to original code

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,10 +8,10 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    // public static final String EXAMPLE = "123, some street";
-    public static final String EXAMPLE = " a/123, Clementi Ave 3, #12-34, 231534 ";
-    //    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in the format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
+     public static final String EXAMPLE = "123, some street";
+  //  public static final String EXAMPLE = " a/123, Clementi Ave 3, #12-34, 231534 ";
+        public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+   // public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in the format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
     public final String value;

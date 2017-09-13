24538b9f63bbfc5ffaaa193f62c6a80ec6137785
Mingrui Tan <mingtui.tan@yahoo.com>
2017-09-13 12:58:05 +0800

    improve code quality

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
-    public static final String MESSAGE_USAGE = "sort: Displays all persons in the address book as a list in alphabetical order.\nExample: sort";
-
-    public SortCommand() {
-    }
+    public static final String MESSAGE_USAGE = "sort: Displays all persons in the address book " +
+            "as a list in alphabetical order.\nExample: sort";
 
     public CommandResult execute() {
         List var1 = this.addressBook.getAllPersons().sortedListView();

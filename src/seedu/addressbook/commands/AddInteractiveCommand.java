676c1de33cf4cda3de5898fba599d4f1f6e9fb1b
Liu Hang <liuhang0213@gmail.com>
2017-09-14 12:38:51 +0800

    Makes it optional for user to enter tags

--- src/seedu/addressbook/commands/AddInteractiveCommand.java
+++ src/seedu/addressbook/commands/AddInteractiveCommand.java
@@ -2,19 +2,12 @@ package seedu.addressbook.commands;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
 
-import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
 import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.data.tag.Tag;
-import seedu.addressbook.data.tag.UniqueTagList;
 import seedu.addressbook.parser.Parser;
 import seedu.addressbook.ui.TextUi;
 
@@ -53,10 +46,14 @@ public class AddInteractiveCommand extends AddCommand {
             generatedCommand += generateCommandFromInput(infoClass, input);
         }
 
+        // Tag is not compulsory; ask user first whether they need tags
+        ui.showToUser("Do you want to add tags for this person?");
         // Tags may be added multiple times; ask user to input a space-separated list for convenience
-        String tagsInput = ui.promptUserInput("Please enter tags for person (separate multiple tags with spaces):");
-        for (String tag : tagsInput.trim().split(" ")) {
-            generatedCommand += generateCommandFromInput(Tag.class, tag);
+        if (ui.isUserAnswerYes()) {
+            String tagsInput = ui.promptUserInput("Please enter tags for person (separate multiple tags with spaces):");
+            for (String tag : tagsInput.trim().split(" ")) {
+                generatedCommand += generateCommandFromInput(Tag.class, tag);
+            }
         }
 
         ui.enqueueCommand(generatedCommand);
@@ -65,16 +62,18 @@ public class AddInteractiveCommand extends AddCommand {
 
     private String generateCommandFromInput(Class<?> infoClass, String input) {
         String generatedCommandSnippet = " ";
+        // It would be more user-friendly to validate input here instead of returning error after all the steps
+        // However due to time constraint we leave the validation to AddCommand
+
         try {
             infoClass.getMethod("isPrivate");
-            String isPrivateInput = ui.promptUserInput("Set this field as private? ([Y] for yes): ").trim().toUpperCase();
-            // It would be more user-friendly to validate input here instead of returning error after all the steps
-            // However due to time constraint we leave the validation to AddCommand
-            Boolean isPrivate = Objects.equals(isPrivateInput, "Y");
+            ui.showToUser("Set this field as private?");
+            Boolean isPrivate = ui.isUserAnswerYes();
             generatedCommandSnippet += (isPrivate) ? "p" : "";
         } catch (NoSuchMethodException | SecurityException e) {
             // Should only happen for Name and Tag class
         }
+
         generatedCommandSnippet += Parser.PERSON_DATA_PREFIXES.get(infoClass.getSimpleName());
         generatedCommandSnippet += input;
         return generatedCommandSnippet;

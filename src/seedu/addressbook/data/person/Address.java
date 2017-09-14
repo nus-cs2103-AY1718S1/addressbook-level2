df74bdf11273ccc27b861bf5f00e3cab30ca7bac
nj <e0053204@u.nus.edu>
2017-09-15 00:31:41 +0800

    add changes to tests

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,19 +9,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    private Block block;
-    private Street street;
-    private Unit unit;
-    private PostalCode postalCode;
-
     public static final String EXAMPLE = "123, some street, #11-11, 101010";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Please enter address in this format:\n"
-                                                + "Block, street, unit, postal code\n"
-                                                + "Postal code may be omitted";
-                                                //"Person addresses can be in any format";
-    //public static final String ADDRESS_VALIDATION_REGEX = ".+";
+                                                + "BLOCK, STREET, UNIT, POSTAL_CODE";
+    public static final String ADDRESS_INVALID = "|| Format of address entered is invalid";
     public static final String ADDRESS_VALIDATION_REGEX = "(.+,)(.+,)(.+,)(.*)";
 
+    private Block block = null;
+    private Street street = null;
+    private Unit unit = null;
+    private PostalCode postalCode = null;
     public final String value;
     private boolean isPrivate;
 
@@ -34,44 +31,28 @@ public class Address {
         String trimmedAddress = address.trim();
         this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
+            System.out.println(ADDRESS_INVALID);
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        //this.value = trimmedAddress;
+        this.value = trimmedAddress;
 
+        /**
+        * Split and save parts of address to respective classes: Block, Street, Unit, PostalCode
+        */
         Scanner sc = new Scanner(trimmedAddress).useDelimiter("\\s*,\\s*");
         this.block = new Block(sc.next());
         this.street = new Street(sc.next());
         this.unit = new Unit(sc.next());
 
-        String output = block.getBlock()+", "+street.getStreet()+", "+unit.getUnit()+", ";
         if (sc.hasNext()){
             this.postalCode = new PostalCode(sc.next());
-            output = output + postalCode.getPostalCode();
         }
-        else
-            this.postalCode = null;
-
-        //String output = block.getBlock()+", "+street.getStreet()+", "+unit.getUnit()+", "+postalCode.getPostalCode();
-
-        this.value = output;
 
     }
-    /*
-    public Address(String address, boolean isPrivate) throws IllegalValueException {
-        String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
-            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        }
-        this.value = trimmedAddress;
-    }
-    */
 
-    /**
-     * Returns true if a given string is a valid person address.
-     */
     public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
+
+        return (test.matches(ADDRESS_VALIDATION_REGEX));
     }
 
     @Override

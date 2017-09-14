48b6a004f3b4c1e522379ecfcf6130d26e0a8edb
Henning <henningtaufrederiksen@gmail.com>
2017-09-14 12:59:15 +0800

    added sublist feature

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -1,9 +1,10 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 import java.util.List;
-
+import java.util.Scanner;
 
 /**
  * Lists all persons in the address book to the user.
@@ -13,13 +14,30 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Displays the desired number of persons in the address book as a list with index numbers. If no" +
+            "integer is given, all persons will be displayed.\n"
             + "Example: " + COMMAND_WORD;
 
 
     @Override
     public CommandResult execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many persons should be displayed? ");
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        if (scanner.hasNextInt()) {
+            try {
+                int i = scanner.nextInt();
+                List<ReadOnlyPerson> subListPersons = allPersons.subList(0, i);
+
+            } catch (IndexOutOfBoundsException ie) {
+                return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+
+        }
+        else {
+            return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+        }
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }

de6da284b75bf12a76821aac22e2da1bb75ec2b8
Houjisan <hongjing10188@gmail.com>
2017-09-14 13:34:41 +0900

    Update Contact class and make Phone extend Contact

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,16 +1,26 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 /**
  * Common class for a person's personal details
  */
 public class Contact {
 
-    protected final String value;
+    public final String value;
     protected boolean isPrivate;
     
-    protected Contact(boolean isPrivate) {
+    protected Contact(String value, boolean isPrivate, String validationRegex, String msgConstraints) throws IllegalValueException {
         this.isPrivate = isPrivate;
-        value = "";
+        String trimmedValue = value.trim();
+        if (!isValid(trimmedValue, validationRegex)) {
+            throw new IllegalValueException(msgConstraints);
+        }
+        this.value = trimmedValue;
+    }
+
+    public static boolean isValid(String test, String validationRegex) {
+        return test.matches(validationRegex);
     }
 
     @Override

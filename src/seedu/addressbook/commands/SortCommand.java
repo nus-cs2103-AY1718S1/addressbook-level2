81a00ac16ec112c1a94be39ea5ff00b6800e66b8
basyiro <bashbashbasyir@hotmail.com>
2017-09-14 22:40:37 +0800

    Added SortCommand

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.commands;
+
+import java.util.ArrayList;
+
+
+public class SortCommand extends Command{
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts all persons according to alphabetical order by name.\n "
+            + "Example: " + COMMAND_WORD;
+
+
+}
+
+

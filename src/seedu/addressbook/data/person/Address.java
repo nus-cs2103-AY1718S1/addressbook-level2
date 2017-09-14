4cf6f2ffcd9480d304403b00b603d2927bdda816
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-09-14 15:39:32 +0800

    [W4.3d] LO-ImplementClasses +Added subclass for Address +Modified sxample for Address format

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,8 +8,10 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "123, some street, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be entered in the following format \n"
+            + "a/BLOCK, STREET, UNIT, POSTAL_CODE \n"
+            + "a/123, Clementi Ave 3, #12-34, 231534";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
     public final String value;
---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+
+    private String block;
+
+    private void Block(String block){
+        this.block = block;
+    }
+
+    public String getBlock(){
+        return this.block;
+    }
+
+}

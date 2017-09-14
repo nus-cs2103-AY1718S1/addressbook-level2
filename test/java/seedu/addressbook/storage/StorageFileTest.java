d9ada647f77b67724f03ca48fb2bd91f6a775fba
Wong Zeng Feng <e0031529@u.nus.edu>
2017-09-14 12:21:28 +0800

    make changes to I/O

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -25,7 +25,7 @@ import static seedu.addressbook.util.TestUtil.assertTextFilesEqual;
 import static seedu.addressbook.util.TestUtil.assertFileDoesNotExist;
 
 public class StorageFileTest {
-    private static final String TEST_DATA_FOLDER = "addressbook-level2/test/data/StorageFileTest";
+    private static final String TEST_DATA_FOLDER = "test/data/StorageFileTest";
     private static final String NON_EXISTANT_FILE_NAME = "ThisFileDoesNotExist.xml";
 
     @Rule

5b2ae233b5407590c4a6a3842a6d2a5d2bc56e40
Joshua <joshua.nkj@gmail.com>
2017-09-14 21:32:46 +0800

    UserGuide.md: Added guide for sort command.

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,8 +1,11 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.data.person.Person;
 
 import java.util.List;
+import java.util.Collections;
 
 
 /**
@@ -13,13 +16,17 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a sorted list with index numbers.\n"
+            + ": Displays all persons in the address book as a sorted list by name.\n"
             + "Example: " + COMMAND_WORD;
 
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        UniquePersonList allPersons = addressBook.getAllPersons();
+        UniquePersonList allPersonsSorted = new UniquePersonList();
+        for (Person currPerson : allPersons){
+//            if curr
+        }
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 

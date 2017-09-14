97e199eab9c6ae92bcf3d5568574007d05211b01
minato <dmc3hpcity@gmail.com>
2017-09-14 21:46:34 +0800

    Person class: add instance variable sequenceNumber and class variable nextSequenceNumber

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -15,6 +15,9 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
+
     private final UniqueTagList tags;
     /**
      * Assumption: Every field must be present and not null.
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = Person.nextSequenceNumber;
+        Person.nextSequenceNumber += 1;
     }
 
     /**
@@ -84,4 +89,7 @@ public class Person implements ReadOnlyPerson {
         return getAsTextShowAll();
     }
 
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
 }

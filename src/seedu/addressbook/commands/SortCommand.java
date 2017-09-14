c93a252b01b49c693216c73121685d1c40fbad7a
Yuchen He <yuchenhe@201-51.priv23.nus.edu.sg>
2017-09-15 00:29:50 +0800

    change the description of SortCommand class

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -5,10 +5,10 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 import java.util.List;
 
 public class SortCommand extends Command {
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Sort persons by name and displays all persons in the address book as a list with index numbers.\n"
             + "Example: " + COMMAND_WORD;
 
 

0e7b75b05b1f97d5c6b4e1338e000e0daf5fcb81
Alex Ang <E0032221@u.nus.edu>
2017-09-14 12:26:56 +0800

    Port level 1 executeUndo and executeRedo into a new command
    Adapted level1 executeUndo and executeRedo as a new command for level2

--- src/seedu/addressbook/commands/RedoCommand.java
+++ src/seedu/addressbook/commands/RedoCommand.java
@@ -1,4 +1,44 @@
 package seedu.addressbook.commands;
 
-public class RedoCommand {
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.state.ApplicationHistory;
+import seedu.addressbook.state.ApplicationState;
+
+import java.util.List;
+
+
+public class RedoCommand extends Command {
+
+    public static final String COMMAND_WORD = "redo";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Redoes the previous undo operation. "
+            + "Redo history will be cleared on application exit.";
+    private static final String MESSAGE_SUCCESS = "%1$s\n\nSuccessfully completed redo operation. "
+            + "Changes to Address Book:\n%2$s";
+    
+    private ApplicationHistory applicationHistory;
+
+    public RedoCommand(ApplicationHistory applicationHistory) {
+        this.applicationHistory = applicationHistory;
+    }
+
+    @Override
+    public CommandResult execute() {
+        ApplicationState saveState = getCurrentApplicationState();
+        ApplicationState loadState = applicationHistory.popRedoHistory();
+        final List<ReadOnlyPerson> loadPersonListing = loadState.getListingInState();
+        try {
+            applicationHistory.loadNewApplicationState(loadState, addressBook);
+            applicationHistory.addStateAfterSuccessfulOperation();
+            return new CommandResult(getMessageForSuccessfulUndoRedo(MESSAGE_SUCCESS, loadPersonListing, loadState, saveState), loadPersonListing);
+            
+        } catch (ApplicationHistory.EmptyRedoHistoryException erhe) {
+            return new CommandResult(String.format(erhe.getMessage(), COMMAND_WORD));
+            
+        } catch (ApplicationHistory.LoadStateException lse) {
+            applicationHistory.pushRedoHistory(loadState);
+            return new CommandResult(String.format(lse.getMessage(), COMMAND_WORD));
+            
+        }
+    }
 }
--- src/seedu/addressbook/commands/UndoCommand.java
+++ src/seedu/addressbook/commands/UndoCommand.java
@@ -1,4 +1,44 @@
 package seedu.addressbook.commands;
 
-public class UndoCommand {
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.state.ApplicationHistory;
+import seedu.addressbook.state.ApplicationState;
+
+import java.util.List;
+
+
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undoes the previous operation. "
+            + "Undo history will be cleared on application exit.";
+    private static final String MESSAGE_SUCCESS = "%1$s\n\nSuccessfully completed undo operation. "
+            + "Changes to Address Book:\n%2$s";
+    
+    private ApplicationHistory applicationHistory;
+
+    public UndoCommand(ApplicationHistory applicationHistory) {
+        this.applicationHistory = applicationHistory;
+    }
+
+    @Override
+    public CommandResult execute() {
+        ApplicationState saveState = getCurrentApplicationState();
+        ApplicationState loadState = applicationHistory.popHistory();
+        final List<ReadOnlyPerson> loadPersonListing = loadState.getListingInState();
+        try {
+            applicationHistory.loadNewApplicationState(loadState, addressBook);
+            applicationHistory.addRedoStateAfterSuccessfulUndo();
+            return new CommandResult(getMessageForSuccessfulUndoRedo(MESSAGE_SUCCESS, loadPersonListing, loadState, saveState), loadPersonListing);
+            
+        } catch (ApplicationHistory.EmptyHistoryException ehe) {
+            return new CommandResult(String.format(ehe.getMessage(), COMMAND_WORD));
+            
+        } catch (ApplicationHistory.LoadStateException lse) {
+            applicationHistory.pushHistory(loadState);
+            return new CommandResult(String.format(lse.getMessage(), COMMAND_WORD));
+            
+        }
+    }
 }

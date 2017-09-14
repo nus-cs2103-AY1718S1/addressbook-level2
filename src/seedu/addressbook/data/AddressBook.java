bf0c36249f4cff52eb1b63239dfaf916a890b284
DESKTOP-9RFF1NR\Ernest Ong YuHeng <E0031105@u.nus.edu>
2017-09-14 18:26:53 +0800

    SortCommand
    Updated User Guide
    
    Files affected/changed accordingly:
    HelpCommand
    AddressBook
    Name
    UniquePersonList
    Parser
    expected.txt
    input.txt
    ParserTest

--- src/seedu/addressbook/data/AddressBook.java
+++ src/seedu/addressbook/data/AddressBook.java
@@ -121,6 +121,13 @@ public class AddressBook {
         return new UniqueTagList(allTags);
     }
 
+    /**
+     * Sort the people found in addressbook in an alphabetic order
+     */
+    public void sort() {
+        allPersons.sort();
+    }
+
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object

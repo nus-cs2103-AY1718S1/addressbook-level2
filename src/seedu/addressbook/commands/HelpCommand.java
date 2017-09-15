934b6f0d7011bd90506afcfe9a7b6e562a00b841
srishag <srishti.23997@gmail.com>
2017-09-15 14:10:29 +0800

    passed

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -21,9 +21,9 @@ public class HelpCommand extends Command {
                 + "\n" + ClearCommand.MESSAGE_USAGE
                 + "\n" + FindCommand.MESSAGE_USAGE
                 + "\n" + ListCommand.MESSAGE_USAGE
+                + "\n" + SortCommand.MESSAGE_USAGE
                 + "\n" + ViewCommand.MESSAGE_USAGE
                 + "\n" + ViewAllCommand.MESSAGE_USAGE
-                + "\n" + SortCommand.MESSAGE_USAGE
                 + "\n" + HelpCommand.MESSAGE_USAGE
                 + "\n" + ExitCommand.MESSAGE_USAGE
         );

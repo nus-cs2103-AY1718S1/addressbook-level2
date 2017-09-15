f1c027498dc6696618987fe4d5d9342324a56efe
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-09-15 13:32:56 +0800

    add Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,55 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+
+    public static String EXAMPLE = "";
+    public static String MESSAGE_CONSTRAINTS = "";
+    public static String VALIDATION_REGEX = "";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given information.
+     *
+     * @throws IllegalValueException if given information string is invalid.
+     */
+    public Contact(String information, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedInfo = information.trim();
+        if (!isValidInfo(trimmedInfo)) {
+            throw new IllegalValueException(MESSAGE_CONSTRAINTS);
+        }
+        this.value = trimmedInfo;
+    }
+
+    /**
+     * Returns true if the given string is a piece of valid information.
+     */
+    public static boolean isValidInfo(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}

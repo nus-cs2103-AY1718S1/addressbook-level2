437c652bd153488fe039196f968bb417615f4b2d
Alex Ang <E0032221@u.nus.edu>
2017-09-15 08:56:22 +0800

    Fix bug found in ApplicationHistory
    pushRedoHistory behaviour incorrect. Amended.

--- src/seedu/addressbook/state/ApplicationHistory.java
+++ src/seedu/addressbook/state/ApplicationHistory.java
@@ -104,5 +104,5 @@ public class ApplicationHistory {
         return redoStack.pop();
     }
 
-    public void pushRedoHistory(ApplicationState applicationState) { historyStack.push(applicationState); }
+    public void pushRedoHistory(ApplicationState applicationState) { redoStack.push(applicationState); }
 }

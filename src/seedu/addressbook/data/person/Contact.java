8fa9b6fac271e4cc9b866e128c5fc20434c4e7ec
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-09-15 16:55:32 +0800

    modify Contact class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,37 +1,22 @@
 package seedu.addressbook.data.person;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import seedu.addressbook.data.exception.IllegalValueException;
 
-public class Contact {
-
-    public static String EXAMPLE = "";
-    public static String MESSAGE_CONSTRAINTS = "";
-    public static String VALIDATION_REGEX = "";
 public abstract class Contact {
 
-    public final String value;
-    private boolean isPrivate;
-
+    private String value;
+    private Boolean isPrivate;
     /**
      * Validates given information.
      *
      * @throws IllegalValueException if given information string is invalid.
      */
-    public Contact(String information, boolean isPrivate) throws IllegalValueException {
+    public Contact(String value, Boolean isPrivate){
+        this.value = value;
         this.isPrivate = isPrivate;
-        String trimmedInfo = information.trim();
-        if (!isValidInfo(trimmedInfo)) {
-            throw new IllegalValueException(MESSAGE_CONSTRAINTS);
-        }
-        this.value = trimmedInfo;
     }
 
-    /**
-     * Returns true if the given string is a piece of valid information.
-     */
-    public static boolean isValidInfo(String test) {
-        return test.matches(VALIDATION_REGEX);
-    }
 
     @Override
     public String toString() {

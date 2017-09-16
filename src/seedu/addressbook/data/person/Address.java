69f5bed18805a6d1b4743578e3b7439b167c85c2
LAPTOP-JJSFIV9M\Zaheer <e0031513@u.nus.edu>
2017-09-16 10:50:04 +0800

    amended test I/O

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street, some postal code";
+    public static final String EXAMPLE = "123, some street, some unit, some postal code";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be in format: BLOCK, STREET, UNIT, POSTAL_CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 

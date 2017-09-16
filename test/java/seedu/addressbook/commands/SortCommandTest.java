fe52ef4d2447d277492497fd478531a94720803b
Icehawk <e0015833@u.nus.edu>
2017-09-16 23:03:32 +0800

    Update SortCommandTest.java to include class SortCommand during test

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -2,23 +2,12 @@ package seedu.addressbook.commands;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+
 
 import org.junit.Test;
 
-import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.tag.UniqueTagList;
 import seedu.addressbook.util.TestUtil;
 import seedu.addressbook.util.TypicalPersons;
 
@@ -26,10 +15,13 @@ public class SortCommandTest {
     private TypicalPersons td = new TypicalPersons();
 
     private AddressBook unsortedAddressBook = td.getUnsortedAddressBook();
+    private List unsortedPersonsList = TestUtil.createList(td.bill, td.dan, td.candy, td.amy);
 
     @Test
     public void execute_sortCompleted_returnsAlphabeticalOrder() throws Exception {
-        assertListSorted(unsortedAddressBook);
+        SortCommand sortTest = new SortCommand();
+        sortTest.addressBook = unsortedAddressBook;
+        assertListSorted(sortTest.addressBook);
     }
 
     private void assertListSorted (AddressBook unsortedAddressBook) {

e21202b3fcdd098645156f8e778712b780ee494b
Pj Poh <e0037036@u.nus.edu>
2017-09-28 22:14:55 +0800

    Import only relevant packages in java.util

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,6 +1,11 @@
 package seedu.addressbook.data.person;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;

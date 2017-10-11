54bf2497d0c7a7f6cd1b0371980471b8656ce5e8
Jelena <neohuilingjelena@gmail.com>
2017-10-11 21:55:56 +0800

    Main.java: print all taggings before exit

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -74,6 +74,7 @@ public class Main {
 
     /** Prints the Goodbye message and exits. */
     private void exit() {
+        addressBook.printAllTaggings();
         ui.showGoodbyeMessage();
         System.exit(0);
     }
